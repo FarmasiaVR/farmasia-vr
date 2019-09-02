78436122b83006f0c03b51bcf090c4659526a1f4
porrasm <31691452+porrasm@users.noreply.github.com>
2019-09-02 13:25:40 +0300

    Block demo

---
+++ Assets/Scripts/BlockMover.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMover : MonoBehaviour {
+
+    #region fields
+    [SerializeField]
+    private float force = 5;
+
+    private Vector2 movement;
+
+    // Physics component
+    private Rigidbody rb;
+    #endregion
+
+    // Called on object spawn
+    private void Awake() {
+
+    }
+
+    // Called before first update
+    private void Start() {
+
+        // Get physics component
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Called every frame
+    private void Update() {
+        CheckInput();
+        Move();
+    }
+
+    private void CheckInput() {
+
+        movement = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W)) {
+            movement.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            movement.y -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            movement.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            movement.x += 1;
+        }
+
+    }
+
+    private void Move() {
+
+        rb.AddForce(Vector3.forward * movement.y * force);
+        rb.AddForce(Vector3.right * movement.x * force);
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    #region fields
+    private Transform block;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start() {
+        // get block
+        block = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(block.position.x, transform.position.y, block.position.z);
+    }
+}
