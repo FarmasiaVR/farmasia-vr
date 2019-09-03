e49b20fdc71ff700ac900dda368a82cc426c14bb
Mirex97 <Mirex97@gmail.com>
2019-09-03 12:37:24 +0300

    Upgraded Demo Room

---
+++ Assets/Scripts/RoomFunctions/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ui/UIWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIWriter : MonoBehaviour
+{
+    Canvas canvas;
+
+    public String toggleChild(String childName) {
+        GameObject child = returnChild(childName);
+    }
+
+    public String writeToName(String childName) {
+        
+    }
+
+    private GameObject returnChild(String childName) {
+        GameObject child = canvas.gameObject.transform.Find("childName");
+        return child;
+    }
+
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
+
+    void Update()
+    {
+        
+    }
+}
