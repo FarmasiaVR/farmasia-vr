636a16745dc8c311a39fcbdaec07695b7a6d637e
porrasm <31691452+porrasm@users.noreply.github.com>
2019-09-03 12:18:02 +0300

    Add Scripts component

---
+++ Assets/Scripts/Utilities/Scripts.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : MonoBehaviour {
+
+    #region fields
+    private static Scripts instance;
+    public static Scripts Instance {
+        get {
+            if (instance == null) {
+                GameObject g = GameObject.FindGameObjectWithTag("Scripts");
+
+                if (g == null) {
+                    throw new System.Exception("Scripts object not present in the scene");
+
+                }
+
+                instance = g.GetComponent<Scripts>();
+
+                if (instance == null) {
+                    throw new System.Exception("Scripts object not present in the scene");
+
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    #endregion
+
+    public static T GetScriptComponent<T>() {
+        return Instance.GetComponent<T>();
+    }
+
+
+}
