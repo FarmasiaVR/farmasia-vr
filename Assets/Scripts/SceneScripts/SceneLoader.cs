39c356182523993b684937635765b186035d56bc
Mirex97 <Mirex97@gmail.com>
2019-09-03 15:56:51 +0300

    Scene Swapping

---
+++ Assets/Scripts/SceneScripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader: MonoBehaviour
+{
+    public string sceneLocation = "_Scenes/MembraneFiltering";
+
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(AsyncSceneLoader());
+        }
+    }
+
+    IEnumerator AsyncSceneLoader()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneLocation);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        
+    }
+}
