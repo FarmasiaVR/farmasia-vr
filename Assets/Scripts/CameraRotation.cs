f8388a3aa1634c09f99e693760beb24c43ad165c
Mirex97 <Mirex97@gmail.com>
2019-09-05 22:48:22 +0300

    Room layout improvements and removal of SceneSwapping

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+
+    enum Axis { X, Y, Z };
+
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            float h = horizontalSpeed * Input.GetAxis("Mouse X");
+            float v = verticalSpeed * Input.GetAxis("Mouse Y");
+            transform.eulerAngles = new Vector3(getAxis(Axis.X, -v), getAxis(Axis.Y, h), getAxis(Axis.Z, 0));
+        }
+
+    }
+
+    private float getAxis(Axis axe, float rotation) {
+        if (axe == Axis.X) {
+            return transform.eulerAngles.x + rotation;
+        } else if (axe == Axis.Y) {
+            return transform.eulerAngles.y + rotation;
+        } else if (axe == Axis.Z) {
+            return transform.eulerAngles.z + rotation;
+        }
+        return 0;
+    }
+}
--- Assets/Scripts/RoomFunctions/Door.cs
+++ Assets/Scripts/RoomFunctions/Door.cs
@@ -2,40 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
-{
+public class Door : MonoBehaviour {
     UIWriter writer;
+    public GameObject warpPoint;
+    private GameObject player = null;
 
 
-    void OnTriggerEnter(Collider collision)
-    {
-        
+    void OnTriggerEnter(Collider collision) {
+
         if (collision.gameObject.name == "Player") {
-            
-            writer.toggleChild("Prompt Field");
 
+            writer.toggleChild("Prompt Field");
+            player = collision.gameObject;
 
         }
     }
 
-    void OnTriggerExit(Collider collision)
-    {
+    void OnTriggerExit(Collider collision) {
         if (collision.gameObject.name == "Player") {
-            
-            writer.toggleChild("Prompt Field");
 
+            writer.toggleChild("Prompt Field");
+            player = null;
 
         }
     }
 
-    
-    void Start()
-    {
+
+    void Start() {
         writer = GameObject.Find("Canvas").GetComponent<UIWriter>();
     }
 
-    void Update()
-    {
-        
+    void Update() {
+        if (player) {
+            if (Input.GetKeyUp(KeyCode.F)) {
+                player.transform.position = warpPoint.transform.position;
+            }
+        }
     }
 }
