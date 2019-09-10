402312c0d056c15c6100f92b3af47863078ee248
Mirex97 <Mirex97@gmail.com>
2019-09-10 03:10:57 +0300

    Progress System (WIP)

---
+++ Assets/Scripts/ProgressSystem/ProgressManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressManager : MonoBehaviour {
+    public static ProgressManager Instance { get; private set; }
+
+    private TaskFactory taskFactory;
+
+    public ITask currentTask;
+
+
+
+    private void Awake() {
+        if (Instance != null && Instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void Start() {
+        taskFactory = new TaskFactory();
+        currentTask = taskFactory.GetTask(TaskType.SelectTools);
+
+    }
+
+}
