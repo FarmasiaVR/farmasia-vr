6e8ca079fe963b252f75b2e54ff9c70e4bb10693
porrasm <31691452+porrasm@users.noreply.github.com>
2019-09-10 16:49:30 +0300

    Merge problems

--- Assets/Scripts/Interactables/TestStaticInteractable.cs
+++ Assets/Scripts/Interactables/TestStaticInteractable.cs
@@ -7,12 +7,12 @@ public class TestStaticInteractable : Interactable {
     protected override void Start() {
         base.Start();
         type = GrabType.Interactable;
-        gameObject.GetComponent<GeneralItem>().EnableFlags(ItemState.Status.Clean);
+        gameObject.GetComponent<GeneralItem>().SetFlags(true, ItemState.Status.Clean);
     }
 
     public override void Interact(Hand hand) {
         base.Interact(hand);
-        gameObject.GetComponent<GeneralItem>().DisableFlags(ItemState.Status.Clean);
+        gameObject.GetComponent<GeneralItem>().SetFlags(false, ItemState.Status.Clean);
         Logger.Print("Clean", gameObject.GetComponent<GeneralItem>().GetFlag(ItemState.Status.Clean));
     }
 }
