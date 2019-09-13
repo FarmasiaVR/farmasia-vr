479b209e4a9b1d35453262bd0f4f6f793dcc252a
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-09-13 12:45:26 +0300

    Move deployment scripts to Deploy directory

--- Deploy/build.sh
+++ Deploy/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+PROJ_DIR=".."
 EXECUTABLE="farmasia-vr.exe"
 BUILD_TARGET="Windows64"
-BUILD_DIR="Build/${BUILD_TARGET}/"
+BUILD_DIR="${PROJ_DIR}/Build/${BUILD_TARGET}"
 LOG_FILE="${BUILD_DIR}/build-$(date +%d-%b-%Y).log" # '-' for stdout
 
 UNITY_VERSION="2019.2.3f1"
 UNITY_PATH="/c/Program Files/Unity/Hub/Editor/${UNITY_VERSION}/Editor/Unity.exe"
 
 # Note that '-nographics' should be removed if GI (Global Illumination) is needed
-UNITY_ARGS="-batchmode -nographics -projectPath . -build${BUILD_TARGET}Player ${BUILD_DIR}/${EXECUTABLE} -quit -logFile ${LOG_FILE}"
+UNITY_ARGS="-batchmode -nographics -projectPath ${PROJ_DIR} -build${BUILD_TARGET}Player ${BUILD_DIR}/${EXECUTABLE} -quit -logFile ${LOG_FILE}"
 
 echo "--> Clearing build directory"
 mkdir -p ${BUILD_DIR}
@@ -17,7 +18,7 @@ rm -r ${BUILD_DIR}/*
 
 # Temp/ is left if the previous build failed for some reason
 echo "--> Deleting temp directory"
-[ -d Temp ] && rm -r Temp
+[ -d ${PROJ_DIR}/Temp ] && rm -r ${PROJ_DIR}/Temp
 
 echo "--> Creating Unity build"
 "${UNITY_PATH}" ${UNITY_ARGS} && echo "Done." && exit
