fa902f74f873a2d771057932de9602cf53d387fa
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-09-16 10:53:21 +0300

    Update paths in upload.sh

--- Deploy/upload.sh
+++ Deploy/upload.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ZIP_FILE="farmasia-vr.zip"
-BUILD_DIR="Build/Windows64"
-OUTPUT_DIR="Build"
+PROJ_DIR=".."
+BUILD_DIR="${PROJ_DIR}/Build/Windows64"
+OUTPUT_DIR="${PROJ_DIR}/Build"
 
-echo ":: Zipping build directory"
+echo "--> Zipping build directory"
 zip -r "${OUTPUT_DIR}/${ZIP_FILE}" "${BUILD_DIR}" >/dev/null 2>&1
 
 echo "The zip can be found at $(readlink -f ${OUTPUT_DIR}/${ZIP_FILE})"
