19936c212643502a1921c8f79cd7044a4d85fc39
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-09-16 12:13:26 +0300

    Add drive upload to upload.sh

--- Deploy/upload.sh
+++ Deploy/upload.sh
@@ -5,9 +5,35 @@ PROJ_DIR=".."
 BUILD_DIR="${PROJ_DIR}/Build/Windows64"
 OUTPUT_DIR="${PROJ_DIR}/Build"
 
-echo "--> Zipping build directory"
-zip -r "${OUTPUT_DIR}/${ZIP_FILE}" "${BUILD_DIR}" >/dev/null 2>&1
+check_prerequisites() {
+    echo "--> Checking prerequisites"
+    fail=0
+    which zip 2>/dev/null || { echo "ERROR: zip is not installed!"; fail=1; }
+    which gdrive 2>/dev/null || { echo "ERROR: gdrive is not installed!"; fail=1; }
+    [ ${fail} -eq 1 ] && exit 1
+}
 
-echo "The zip can be found at $(readlink -f ${OUTPUT_DIR}/${ZIP_FILE})"
+create_zip() {
+    echo "--> Zipping build directory"
+    zip -r "${OUTPUT_DIR}/${ZIP_FILE}" "${BUILD_DIR}" >/dev/null 2>&1
+    [ $? -eq 0 ] || { echo "FAIL"; exit 1; }
+}
+
+upload_zip() {
+    echo "--> Uploading zip to Google Drive"
+    prev_upload_id=$(gdrive list | awk 'NR==2 {print $1; exit}')
+
+    # Upload new file if prev_upload_id is empty
+    # Else, update existing file
+    if [ -z "${prev_upload_id}" ]; then
+        gdrive upload "${OUTPUT_DIR}/${ZIP_FILE}"
+    else
+        gdrive update "${prev_upload_id}" "${OUTPUT_DIR}/${ZIP_FILE}"
+    fi
+}
+
+check_prerequisites
+create_zip
+upload_zip
 echo
 echo "Done."
