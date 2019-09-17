cc2c111e038034fc4e5d58eef40cdb92d8d2f664
Veikka <32365239+SirVeggie@users.noreply.github.com>
2019-09-17 15:53:26 +0300

    Add Item Placement

---
+++ Assets/Scripts/ItemPlacement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlacement : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        
+    }
+
+    public void TryPlacement(Transform item) {
+        float angle = Vector3.Angle(item.up, Vector3.up);
+        if (angle > 25f) {
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        rb.velocity = Vector3.zero;
+
+        Destroy(this);
+    }
+}
