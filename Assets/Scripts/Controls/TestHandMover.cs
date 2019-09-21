5976551681354b5e4a93fd0328568679968f656b
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-09-21 21:33:07 +0300

    Refactor TestHandMover.cs

--- Assets/Scripts/Controls/TestHandMover.cs
+++ Assets/Scripts/Controls/TestHandMover.cs
@@ -3,86 +3,122 @@
 public class TestHandMover : MonoBehaviour {
 
     #region fields
-    private bool usingRight;
-
-    private Vector3 movement;
+    private const KeyCode HAND_LEFT = KeyCode.Mouse0;
+    private const KeyCode HAND_RIGHT = KeyCode.Mouse1;
+    private const KeyCode HAND_INTERACT = KeyCode.Mouse1;
+    private const KeyCode MOVE_FORWARD = KeyCode.W;
+    private const KeyCode MOVE_LEFT = KeyCode.A;
+    private const KeyCode MOVE_BACKWARDS = KeyCode.S;
+    private const KeyCode MOVE_RIGHT = KeyCode.D;
+    private const KeyCode MOVE_UP = KeyCode.E;
+    private const KeyCode MOVE_DOWN = KeyCode.Q;
 
     private float handSpeed = 1;
 
     private Hand right, left;
+    private Hand current;
+    private bool isGrabbing;
     #endregion
 
     void Start() {
         right = transform.GetChild(0).GetComponent<Hand>();
         left = transform.GetChild(1).GetComponent<Hand>();
+        current = left;
     }
 
     void Update() {
-        UpdateInteract();
-        UpdateHandStatus();
-        CheckInput();
-        MoveHand();
+        UpdateHands();
+        UpdateMovement();
     }
 
-    private void UpdateInteract() {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && !usingRight) {
-            left.InteractWithObject();
-        } else if (Input.GetKeyDown(KeyCode.Mouse1) && usingRight) {
-            right.InteractWithObject();
+    private void UpdateHands() {
+        if (JustPressed(HAND_LEFT)) {
+            ActivateLeft();
+        } else if (JustPressed(HAND_RIGHT)) {
+            ActivateRight();
+        } else if (JustPressed(HAND_INTERACT)) {
+            Interact();
         }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0) && !usingRight) {
-            left.UninteractWithObject();
-        } else if (Input.GetKeyUp(KeyCode.Mouse1) && usingRight) {
-            right.UninteractWithObject();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse2) && !usingRight) {
-            left.GrabInteract();
-        } else if (Input.GetKeyUp(KeyCode.Mouse2) && usingRight) {
-            right.GrabInteract();
+        if (JustReleased(HAND_LEFT)) {
+            ReleaseLeftObject();
+        } else if (JustReleased(HAND_RIGHT)) {
+            ReleaseRightObject();
         }
     }
 
-    private void UpdateHandStatus() {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && usingRight) {
-            usingRight = false;
-        } else if (Input.GetKeyDown(KeyCode.Mouse1) && !usingRight) {
-            usingRight = true;
-        }
-    }
+    private void UpdateMovement() {
+        Vector3 movement = Vector3.zero;
 
-    private void CheckInput() {
-        movement = Vector3.zero;
-
-        if (Pressing(KeyCode.W)) {
+        if (IsPressed(MOVE_FORWARD)) {
             movement.z++;
         }
-        if (Pressing(KeyCode.S)) {
+        if (IsPressed(MOVE_BACKWARDS)) {
             movement.z--;
         }
 
-        if (Pressing(KeyCode.D)) {
+        if (IsPressed(MOVE_RIGHT)) {
             movement.x++;
         }
-        if (Pressing(KeyCode.A)) {
+        if (IsPressed(MOVE_LEFT)) {
             movement.x--;
         }
 
-        if (Pressing(KeyCode.E)) {
+        if (IsPressed(MOVE_UP)) {
             movement.y++;
         }
-        if (Pressing(KeyCode.Q)) {
+        if (IsPressed(MOVE_DOWN)) {
             movement.y--;
+        }
+
+        current.transform.Translate(movement * handSpeed * Time.deltaTime);
+    }
+
+    private void ActivateLeft() {
+        if (current != left) {
+            current = left;
+            return;
+        }
+        GrabObject();
+    }
+
+    private void ActivateRight() {
+        if (current != right) {
+            current = right;
+            return;
+        }
+        GrabObject();
+    }
+
+    private void GrabObject() {
+        current.InteractWithObject();
+    }
+
+    private void ReleaseLeftObject() {
+        if (current == left) {
+            current.UninteractWithObject();
         }
     }
 
-    private void MoveHand() {
-        Transform hand = usingRight ? right.transform : left.transform;
-        hand.Translate(movement * handSpeed * Time.deltaTime);
+    private void ReleaseRightObject() {
+        if (current == right) {
+            current.UninteractWithObject();
+        }
     }
 
-    private bool Pressing(KeyCode c) {
+    private void Interact() {
+        current?.GrabInteract();
+    }
+
+    private bool IsPressed(KeyCode c) {
         return Input.GetKey(c);
+    }
+
+    private bool JustPressed(KeyCode c) {
+        return Input.GetKeyDown(c);
+    }
+
+    private bool JustReleased(KeyCode c) {
+        return Input.GetKeyUp(c);
     }
 }
