8fffddda3ca273035e44f4bdc4955b759bb78ca6
Mirex97 <Mirex97@gmail.com>
2019-09-22 16:49:09 +0300

    Fixed methods and added finishing progress

--- Assets/Scripts/ProgressSystem/ProgressManager.cs
+++ Assets/Scripts/ProgressSystem/ProgressManager.cs
@@ -4,17 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProgressManager : MonoBehaviour {
-
-    #region fields
+    #region Fields
     public static ProgressManager Instance { get; private set; }
-    [SerializeField]
     private List<ITask> activeTasks;
     private List<TaskType> doneTypes;
     private List<ITask> doneTasks;
     private ScoreCalculator calculator;
+    private bool isFinished = false;
+    private float finishTimer = 0.0f;
+    private float waitTime = 5.0f;
     #endregion
 
-    #region initialization
+    #region Initialisation
     /// <summary>
     /// Creates a singleton of ProgressManager.
     /// </summary>
@@ -38,7 +39,20 @@ public class ProgressManager : MonoBehaviour {
     }
     #endregion
 
-    #region private methods
+    #region Private Methods
+    /// <summary>
+    /// Once all Tasks are finished, waits for a moment and then finishes progress.
+    /// </summary>
+    private void Update() {
+        if (isFinished) {
+            finishTimer += Time.deltaTime;
+            if (finishTimer >= waitTime) {
+                FinishProgress();
+                isFinished = false;
+            }
+        }
+    }
+
     /// <summary>
     /// Creates a single task from every enum TaskType object.
     /// Adds tasks into currently activeTasks.
@@ -46,20 +60,20 @@ public class ProgressManager : MonoBehaviour {
     private void AddTasks() {
         activeTasks = Enum.GetValues(typeof(TaskType))
             .Cast<TaskType>()
-            .Where(v => !v.Equals(TaskType.MissingItems))
             .Select(v => TaskFactory.GetTask(v))
+            .Where(v => v != null)
             .ToList();
+    }
+
+    /// <summary>
+    /// Once all tasks are finished, FinishProgress is called to create a Congratulation popup.
+    /// </summary>
+    private void FinishProgress() {
+        UISystem.Instance.CreatePopup(0, "Congratulations!\nAll tasks finished", MessageType.Congratulate);
     }
     #endregion
 
-    #region public methods
-    /// <summary>
-    /// Adds task to the current active list.
-    /// </summary>
-    /// <param name="task">Refers to task to be added.</param>
-    public void AddTask(ITask task) {
-        activeTasks.Add(task); 
-    }
+    #region Public Methods
     /// <summary>
     /// Removes task from current active list and adds them to doneTasks list.
     /// Tasks are still active inside doneTasks list!
@@ -68,14 +82,13 @@ public class ProgressManager : MonoBehaviour {
     public void RemoveTask(ITask task) {
         doneTasks.Add(task);
         doneTypes.Add(task.GetTaskType());
-        activeTasks.Remove(task);  
-    }
-    /// <summary>
-    /// Returns list presentation of active tasks.
-    /// </summary>
-    /// <returns>returns activeTasks list.</returns>
-    public List<ITask> GetActiveTasks() {
-        return activeTasks;
+        activeTasks.Remove(task);
+        if (activeTasks.Count == 0) {
+            isFinished = true;
+        } else {
+            Debug.Log("Still " + activeTasks.Count + " left!");
+        }
+
     }
 
     /// <summary>
