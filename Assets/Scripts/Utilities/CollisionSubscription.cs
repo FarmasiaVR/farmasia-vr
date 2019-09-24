ba551ff334fd5d05ffce3c769472fe5c23cb629f
porrasm <31691452+porrasm@users.noreply.github.com>
2019-09-24 14:01:24 +0300

    Finish CollisionSubscription

--- Assets/Scripts/Utilities/CollisionSubscription.cs
+++ Assets/Scripts/Utilities/CollisionSubscription.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+// Maybe add support for multiple subscriptions????
 public class CollisionSubscription : MonoBehaviour {
 
     #region fields
@@ -17,20 +16,54 @@ public class CollisionSubscription : MonoBehaviour {
         T_Stay = stay;
         T_Exit = exit;
     }
+    private void SetCollisionCallbacks(CollisionCallback enter, CollisionCallback stay, CollisionCallback exit) {
+        C_Enter = enter;
+        C_Stay = stay;
+        C_Exit = exit;
+    }
 
     #region Triggers
     private void OnTriggerEnter(Collider collider) {
         T_Enter?.Invoke(collider);
+    }
+    private void OnTriggerStay(Collider collider) {
+        T_Stay?.Invoke(collider);
     }
     private void OnTriggerExit(Collider collider) {
         T_Exit?.Invoke(collider);
     }
     #endregion
 
+    #region Collisions
+    private void OnCollisionEnter(Collision collision) {
+        C_Enter?.Invoke(collision);
+    }
+    private void OnCollisionStay(Collision collision) {
+        C_Stay?.Invoke(collision);
+    }
+    private void OnCollisionExit(Collision collision) {
+        C_Exit?.Invoke(collision);
+    }
+    #endregion
+
+    #region Subscribing
     public static void SubscribeToTrigger(GameObject g, TriggerCallback enter, TriggerCallback stay, TriggerCallback exit) {
 
-        CollisionSubscription coll = g.AddComponent<CollisionSubscription>();
+        CollisionSubscription coll = g.GetComponent<CollisionSubscription>();
+        if (coll == null) {
+            coll = g.AddComponent<CollisionSubscription>();
+        }
 
         coll.SetTriggerCallbacks(enter, stay, exit);
     }
+    public static void SubscribeToCollision(GameObject g, CollisionCallback enter, CollisionCallback stay, CollisionCallback exit) {
+
+        CollisionSubscription coll = g.GetComponent<CollisionSubscription>();
+        if (coll == null) {
+            coll = g.AddComponent<CollisionSubscription>();
+        }
+
+        coll.SetCollisionCallbacks(enter, stay, exit);
+    }
+    #endregion
 }
