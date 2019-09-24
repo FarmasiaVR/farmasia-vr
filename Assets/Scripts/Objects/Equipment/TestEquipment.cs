32638e957a786943fbf262d2651f4eb875e37bb8
Veikka <32365239+SirVeggie@users.noreply.github.com>
2019-09-24 18:38:23 +0300

    Add TestScenes folder and TestEquipment for transfer testing

---
+++ Assets/Scripts/Objects/Equipment/TestEquipment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class TestEquipment : GeneralItem {
+
+    [SerializeField]
+    private GameObject targetObject;
+    private LiquidContainer other;
+    private LiquidContainer container;
+
+    protected override void Start() {
+        base.Start();
+        container = GetComponent<LiquidContainer>();
+        Assert.IsNotNull(container);
+        other = targetObject.GetComponent<LiquidContainer>();
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            container.Transfer(other, 10);
+        }
+    }
+}
