0824abb475f547d20923b8d2097a7224275c15a6
Reko <reko.kalkaja@gmail.com>
2019-09-24 13:43:46 +0300

    added LuerlockTestScene

--- Assets/Scripts/Objects/Equipment/LuerlockAdapter.cs
+++ Assets/Scripts/Objects/Equipment/LuerlockAdapter.cs
@@ -8,7 +8,16 @@ public class LuerlockAdapter : GeneralItem {
     public GameObject AttachedObject1 {
         get => attachedObject1;
         set {
+            if (attachedObject1 != null)
+            {
+                attachedObject1.transform.parent = null;
+                attachedObject1 = null;
+            }
             attachedObject1 = value;
+
+            attachedObject1.GetComponent<Rigidbody>().isKinematic = true;
+            attachedObject1.transform.SetParent(this.transform);
+            attachedObject1.transform.localPosition = Vector3.zero;
         }
     }
 
@@ -17,7 +26,14 @@ public class LuerlockAdapter : GeneralItem {
     public GameObject AttachedObject2 {
         get => attachedObject2;
         set {
+            if (attachedObject2 != null)
+            {
+                attachedObject2.transform.parent = null;
+                attachedObject2 = null;
+            }
             attachedObject2 = value;
+            attachedObject2.GetComponent<Rigidbody>().isKinematic = true;
+            attachedObject2.transform.SetParent(this.transform);
         }
     }
     #endregion
@@ -25,5 +41,40 @@ public class LuerlockAdapter : GeneralItem {
     protected override void Start() {
         base.Start();
         ObjectType = ObjectType.Luerlock;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collisionObject = collision.gameObject;
+
+        if (collisionObject == AttachedObject1 || collisionObject == AttachedObject2)
+        {
+            Physics.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider>());
+            return;
+        }
+
+        if (collisionObject.GetComponent<GeneralItem>().ObjectType == ObjectType.Syringe)
+        {
+            AttachSyringe(collisionObject);
+        }
+
+        Logger.Print("Luerlock adapter hit something");
+    }
+
+    private void AttachSyringe(GameObject syringe)
+    {
+        if (syringe.GetComponent<GeneralItem>().ObjectType != ObjectType.Syringe ||
+            AttachedObject1 == syringe || AttachedObject2 == syringe)
+        {
+            return;
+        }
+
+        if (AttachedObject1 == null)
+        {
+            AttachedObject1 = syringe;
+        } else if (attachedObject2 == null)
+        {
+            AttachedObject2 = syringe;
+        }
     }
 }
