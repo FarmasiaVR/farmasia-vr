380855dd6591a0a521a7591935afb364ffc31b67
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-10-01 11:36:01 +0300

    Update Daniel's LiquidTransfer scene
    Make use of Medicine bottles instead of
    raw liquid containers.

---
+++ Assets/_Sandbox/Daniel/Scripts/TransferController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class TransferController : MonoBehaviour
+{
+    #region fields
+    [SerializeField]
+    private GameObject source;
+    [SerializeField]
+    private GameObject sink;
+    [SerializeField]
+    private KeyCode transferKey = KeyCode.F;
+    [SerializeField]
+    private KeyCode switchKey = KeyCode.T;
+    #endregion
+
+    private void FixedUpdate() {
+        if (Input.GetKey(transferKey)) {
+            LiquidContainer src = LiquidContainer.FindLiquidContainer(source.transform);
+            LiquidContainer dst = LiquidContainer.FindLiquidContainer(sink.transform);
+            src?.TransferTo(dst, 1);
+        }
+        if (Input.GetKeyDown(switchKey)) {
+            GameObject tmp = source;
+            source = sink;
+            sink = tmp;
+        }
+    }
+}
