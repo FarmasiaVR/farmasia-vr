2e42ecb154568db523543bb38b0010c35d04368e
porrasm <31691452+porrasm@users.noreply.github.com>
2019-10-04 14:03:36 +0300

    Separate item grabbing from Hand into ItemConnector.cs

---
+++ Assets/Scripts/Controls/Hand/Hand.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour {
+
+    #region fields
+    public bool IsGrabbed { get => Connector.IsGrabbed; }
+
+    public SteamVR_Input_Sources HandType { get; private set; }
+
+    private VRHandControls controls;
+
+    public HandCollider coll { get; private set; }
+
+    public Interactable Interactable { get; private set; }
+
+    [SerializeField]
+    private Hand other;
+    public Hand Other { get => other; }
+
+    public ItemConnector Connector { get; private set; }
+    #endregion
+
+    private void Start() {
+        HandType = GetComponent<VRHandControls>().handType;
+        coll = transform.GetChild(0).GetComponent<HandCollider>();
+        controls = GetComponent<VRHandControls>();
+        Connector = new ItemConnector(this);
+    }
+
+    private void Update() {
+        UpdateControls();
+    }
+
+    private void UpdateControls() {
+
+        if (VRInput.GetControlDown(HandType, Controls.Grab)) {
+            InteractWithObject();
+        }
+        if (VRInput.GetControlUp(HandType, Controls.Grab)) {
+            UninteractWithObject();
+        }
+
+        if (VRInput.GetControlDown(HandType, Controls.GrabInteract)) {
+            GrabInteract();
+        }
+        if (VRInput.GetControlUp(HandType, Controls.GrabInteract)) {
+            GrabUninteract();
+        }
+    }
+
+    #region Interaction
+    public void InteractWithObject() {
+        if (IsGrabbed) {
+            Connector.ReleaseItemFromHand();
+            return;
+        }
+
+        Interactable = coll.GetGrab();
+        if (Interactable == null) {
+            return;
+        }
+
+        if (Interactable.Types.IsOn(InteractableType.Grabbable)) {
+            // Grab();
+            Connector.ConnectItemToHand(Interactable);
+        } else if (Interactable.Types.IsOn(InteractableType.Interactable)) {
+            Interactable.Interact(this);
+        }
+    }
+
+    public void UninteractWithObject() {
+        if (IsGrabbed) {
+            if (VRControlSettings.HoldToGrab) {
+                // Release();
+                Connector.ReleaseItemFromHand();
+            }
+        } else if (Interactable != null) {
+            Interactable.Uninteract(this);
+            Interactable = null;
+        }
+    }
+
+    public void GrabInteract() {
+        if (!IsGrabbed) {
+            return;
+        }
+
+        Interactable = coll.GetGrab();
+
+        if (Interactable.Types.AreOn(InteractableType.Grabbable, InteractableType.Interactable)) {
+            Interactable.Interact(this);
+        }
+    }
+
+    public void GrabUninteract() {
+        if (!IsGrabbed) {
+            return;
+        }
+
+        Interactable = coll.GetGrab();
+
+        if (Interactable.Types.AreOn(InteractableType.Grabbable, InteractableType.Interactable)) {
+            Interactable.Uninteract(this);
+        }
+    }
+    #endregion
+    
+    private void OnJointBreak(float breakForce) {
+        Logger.Print("Joint force broken: " + breakForce);
+        Connector.ReleaseItemFromHand();
+    }
+}
---
+++ Assets/Scripts/Controls/Hand/ItemConnector.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+public class ItemConnector {
+
+    #region fields
+    private Hand hand;
+
+    public bool IsGrabbed { get; private set; }
+
+    private Rigidbody GrabbedRigidbody;
+
+    private Vector3 grabOffset;
+    private Vector3 rotOffset;
+
+    private FixedJoint joint;
+    private FixedJoint Joint {
+        get {
+            if (joint == null) {
+                AddJoint(hand.gameObject);
+            }
+            return joint;
+        }
+    }
+    #endregion
+
+    public ItemConnector(Hand hand) {
+        this.hand = hand;
+    }
+
+    #region Hand grab
+    public void ConnectItemToHand(Interactable interactable) {
+
+        GrabbedRigidbody = interactable.GetComponent<Rigidbody>();
+
+        if (GrabbedRigidbody == null) {
+            throw new System.Exception("Interactable had no rigidbody");
+        }
+
+        GrabbedRigidbody.GetComponent<Interactable>().State = InteractState.Grabbed;
+
+        Events.FireEvent(EventType.PickupObject, CallbackData.Object(GrabbedRigidbody.gameObject));
+
+        if (hand.Other.IsGrabbed && hand.Other.Connector.GrabbedRigidbody.gameObject == GrabbedRigidbody.gameObject) {
+            hand.Other.Connector.ReleaseItemFromHand();
+        }
+
+        IsGrabbed = true;
+        InitializeOffset();
+        AttachGrabbedObject();
+    }
+
+    private void InitializeOffset() {
+        grabOffset = GrabbedRigidbody.transform.position - ColliderPosition;
+        rotOffset = GrabbedRigidbody.transform.eulerAngles - ColliderEulerAngles;
+
+        Logger.Print("Grab offset: " + grabOffset);
+    }
+
+    private void AttachGrabbedObject() {
+        Joint.connectedBody = GrabbedRigidbody;
+    }
+    #endregion
+
+    #region Hand release
+    public void ReleaseItemFromHand() {
+        IsGrabbed = false;
+
+        DeattachGrabbedObject();
+
+        if (GrabbedRigidbody == null) {
+            return;
+        }
+
+        GrabbedRigidbody.GetComponent<Interactable>().State = InteractState.None;
+
+        ItemPlacement.ReleaseSafely(GrabbedRigidbody.gameObject);
+
+        GrabbedRigidbody.velocity = VRInput.Skeleton(hand.HandType).velocity;
+        GrabbedRigidbody.angularVelocity = VRInput.Skeleton(hand.HandType).angularVelocity;
+    }
+
+    private void DeattachGrabbedObject() {
+        Joint.connectedBody = null;
+    }
+    #endregion
+
+    #region Helper functions
+    private void AddJoint(GameObject gObject) {
+        joint = gObject.AddComponent<FixedJoint>();
+        joint.breakForce = 20000;
+        joint.breakTorque = 20000;
+    }
+
+    private Vector3 ColliderPosition {
+        get {
+            return hand.transform.GetChild(0).transform.position;
+        }
+    }
+    private Vector3 ColliderEulerAngles {
+        get {
+            return hand.transform.eulerAngles;
+        }
+    }
+    #endregion
+}
