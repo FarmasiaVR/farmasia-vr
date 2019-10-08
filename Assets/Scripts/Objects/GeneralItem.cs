75aad5902f942cbbdbe0935b13746bb66c2888d8
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-10-08 15:08:56 +0300

    Convert ItemState into a simple boolean

--- Assets/Scripts/Objects/GeneralItem.cs
+++ Assets/Scripts/Objects/GeneralItem.cs
@@ -3,26 +3,12 @@
 public class GeneralItem : Grabbable {
 
     #region fields
-    private int flags;
-
     [SerializeField]
     protected ObjectType objectType = ObjectType.None;
     public ObjectType ObjectType { get => objectType; set { objectType = value; } }
 
     [SerializeField]
     private bool isClean;
+    public bool IsClean { get => isClean; private set => isClean = value; }
     #endregion
-
-    protected override void Start() {
-        base.Start();
-        SetFlags(isClean, ItemState.Status.Clean);
-    }
-
-    public void SetFlags(bool value, params ItemState.Status[] statuses) {
-        ItemState.SetFlags(ref flags, value, statuses);
-    }
-
-    public bool GetFlag(ItemState.Status status) {
-        return ItemState.GetFlag(flags, status);
-    }
 }
