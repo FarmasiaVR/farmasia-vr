4d57f269a7b51c04a39e54a84cd7724f4c41bf31
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-10-08 17:27:27 +0300

    Add PipelineManager as constructor parameter

--- Assets/Scripts/Controls/VRPadSwipeDetection.cs
+++ Assets/Scripts/Controls/VRPadSwipeDetection.cs
@@ -13,6 +13,7 @@ public class VRPadSwipeDetection {
     public SwipeCallback OnSwipeUp { get; set; }
     public SwipeCallback OnSwipeDown { get; set; }
     private SteamVR_Input_Sources handType;
+    private PipelineManager pipelineManager;
     private Pipeline timeout;
     private float timeoutSec;
     // Range: [0, 2] (pad values range from [-1, 1])
@@ -23,11 +24,13 @@ public class VRPadSwipeDetection {
     private float deltaX, deltaY;
     #endregion
 
-    /// <param name="handType">True for left hand pad, false for right hand pad</param>
+    /// <param name="manager">The PipelineManager used to update pipelines</param>
+    /// <param name="isLeftHand">True for left hand pad, false for right hand pad</param>
     /// <param name="swipeThreshold">How long the swipe needs to be to trigger the callback, range: [0, 2]</param>
     /// <param name="timeoutSec">How long the swipe is allowed to last before becoming invalid</param>
-    public VRPadSwipeDetection(bool leftHand, float swipeThreshold, float timeoutSec) {
-        this.handType = leftHand ? SteamVR_Input_Sources.LeftHand : SteamVR_Input_Sources.RightHand;
+    public VRPadSwipeDetection(PipelineManager manager, bool isLeftHand, float swipeThreshold, float timeoutSec) {
+        this.pipelineManager = manager;
+        this.handType = isLeftHand ? SteamVR_Input_Sources.LeftHand : SteamVR_Input_Sources.RightHand;
         this.swipeThreshold = Mathf.Min(Mathf.Max(swipeThreshold, 0), 2);
         this.timeoutSec = timeoutSec;
         Reset();
@@ -37,7 +40,7 @@ public class VRPadSwipeDetection {
         if (VRInput.GetControl(handType, ControlType.PadTouch)) {
             if (VRInput.GetControlDown(handType, ControlType.PadTouch)) {
                 isSwiping = true;
-                timeout = G.Instance.Pipeline.New().Delay(timeoutSec).Func(Reset);
+                timeout = pipelineManager.New().Delay(timeoutSec).Func(Reset);
                 return;
             }
 
--- Assets/Scripts/UISystem/UISystem.cs
+++ Assets/Scripts/UISystem/UISystem.cs
@@ -23,7 +23,7 @@ public class UISystem : MonoBehaviour {
     private GameObject currentDescription;
     private MeshRenderer descriptionMesh;
     private Description description;
-    private VRPadSwipeDetection swipe = new VRPadSwipeDetection(true, 0.75f, 0.25f);
+    private VRPadSwipeDetection swipe;
     #endregion
 
     #region TextMesh objects
@@ -49,6 +49,7 @@ public class UISystem : MonoBehaviour {
         arrowRight = GameObject.FindGameObjectWithTag("Arrow (Right)").GetComponent<TextMeshPro>();
         taskNumber = GameObject.FindGameObjectWithTag("TaskNumber").GetComponent<TextMeshPro>();
 
+        swipe = new VRPadSwipeDetection(G.Instance.Pipeline, true, 0.75f, 0.25f);
         swipe.OnSwipeUp = (dy) => SetDescriptionVisibility(true);
         swipe.OnSwipeDown = (dy) => SetDescriptionVisibility(false);
         swipe.OnSwipeLeft = (dx) => NextDescription(false);
