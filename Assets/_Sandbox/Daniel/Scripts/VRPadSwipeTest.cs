5dcc2f8923344ffbc6110c0ace997434b332418d
Farmasia VR <farmasia.vr@gmail.com>
2019-10-08 13:40:53 +0300

    Add VRPadSwipeTest scene to Daniel's Sandbox

---
+++ Assets/_Sandbox/Daniel/Scripts/VRPadSwipeTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class VRPadSwipeTest : MonoBehaviour
+{
+
+    private VRPadSwipeDetection swipe = new VRPadSwipeDetection(SteamVR_Input_Sources.RightHand);
+
+    private void Awake() {
+        swipe.OnSwipeRight = () => { Logger.Print("Yay callback"); };
+    }
+
+    private void Update() {
+        swipe.Update(Time.deltaTime);
+    }
+}
