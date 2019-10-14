60ee08b298962915c9a841a4b3d49e1deb20396d
Veikka <32365239+SirVeggie@users.noreply.github.com>
2019-10-14 15:09:17 +0300

    Quick fix

--- Assets/Scripts/RoomFunctions/ExitDoor.cs
+++ Assets/Scripts/RoomFunctions/ExitDoor.cs
@@ -10,7 +10,7 @@ public class ExitDoor : MonoBehaviour {
         GameObject floor = GameObject.FindWithTag("Floor");
         GameObject trashBin = GameObject.FindWithTag("TrashBin");
         if (String.Equals(G.Instance.Progress.currentPackage.name, "Clean up")) {
-            bool allItemsFromFloorToTrash = (floor.GetComponent<Floor>().droppedItems == trashBin.GetComponent<TrashBin>.ObjectsInArea.Count);
+            bool allItemsFromFloorToTrash = (floor.GetComponent<Floor>().droppedItems == trashBin.GetComponent<TrashBin>().objectsInArea.Count);
             Events.FireEvent(EventType.CleanUp, CallbackData.Boolean(allItemsFromFloorToTrash));
             //finish task Finish
             //quit game
