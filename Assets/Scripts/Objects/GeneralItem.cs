c7995b78a74ae2f715e0d424f5a9d217f473a86e
Veikka <32365239+SirVeggie@users.noreply.github.com>
2019-10-15 14:21:22 +0300

    Add contamination to general item

--- Assets/Scripts/Objects/GeneralItem.cs
+++ Assets/Scripts/Objects/GeneralItem.cs
@@ -7,9 +7,12 @@ public class GeneralItem : Grabbable {
     protected ObjectType objectType = ObjectType.None;
     public ObjectType ObjectType { get => objectType; set { objectType = value; } }
 
-
     [SerializeField]
     private bool isClean;
     public bool IsClean { get => isClean; set => isClean = value; }
     #endregion
+
+    protected virtual void OnCollisionEnter(Collision coll) {
+        if (coll.gameObject.tag == "Floor") IsClean = false;
+    }
 }
