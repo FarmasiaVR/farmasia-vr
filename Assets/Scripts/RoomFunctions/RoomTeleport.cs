15769867bca08ce9984a6a22c16cc9cd63fe553f
Farmasia VR <farmasia.vr@gmail.com>
2019-10-17 13:35:38 +0300

    Added RoomTeleportation, needs tweaking
    Teleportation works but displacement appears in Pass-Through Cabinet
    InverseTransformPoint needs to be changed to something better

--- Assets/Scripts/RoomFunctions/RoomTeleport.cs
+++ Assets/Scripts/RoomFunctions/RoomTeleport.cs
@@ -4,19 +4,19 @@ public class RoomTeleport : MonoBehaviour {
     #region Fields
     [Tooltip("Insert Teleportation Spot for the player here!")]
     [SerializeField]
-    readonly GameObject playerTele;
+    public GameObject playerTele;
     [Tooltip("Insert Pass-Through cabinet teleportation here!")]
     [SerializeField]
-    readonly GameObject passThroughTele;
-
+    public GameObject passThroughTele;
+    [SerializeField]
+    public GameObject playerLocalPoint;
+    [SerializeField]
+    public GameObject passThroughTriggerLocal;
     private PassThroughCabinet cabinet;
     private GameObject player;
     #endregion
 
-    /// <summary>
-    /// Moves player and PassThrough cabinet to next room.
-    /// </summary>
-    public RoomTeleport() {
+    private void Start() {
         cabinet = GameObject.FindGameObjectWithTag("PassThrough (Prep)")?.GetComponent<PassThroughCabinet>();
         player = GameObject.FindGameObjectWithTag("Player");
     }
@@ -27,10 +27,10 @@ public class RoomTeleport : MonoBehaviour {
             return;
         }
         foreach (GameObject cabinetItem in cabinet.GetContainedItems()) {
-            Vector3 localPosition = cabinetItem.transform.InverseTransformPoint(passThroughTele.transform.position);
-            cabinetItem.transform.position = new Vector3(passThroughTele.transform.position.x + localPosition.x, passThroughTele.transform.position.y + localPosition.y, passThroughTele.transform.position.z + localPosition.z);
+            Vector3 localPosition = cabinetItem.transform.InverseTransformPoint(passThroughTriggerLocal.transform.position);
+            cabinetItem.transform.position = new Vector3(passThroughTele.transform.position.x + localPosition.x, passThroughTele.transform.position.y + localPosition.y + 0.1f, passThroughTele.transform.position.z + localPosition.z);
         }
-        Vector3 playerLocalPosition = player.transform.InverseTransformPoint(playerTele.transform.position);
+        Vector3 playerLocalPosition = player.transform.InverseTransformPoint(playerLocalPoint.transform.position);
         player.transform.position = new Vector3(playerTele.transform.position.x + playerLocalPosition.x, playerTele.transform.position.y + playerLocalPosition.y, playerTele.transform.position.z + playerLocalPosition.z);
     }
 }
