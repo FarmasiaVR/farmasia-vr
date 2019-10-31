15182af7097a0659cd5c8d6a509e59fad23dc4fa
Mirex <Mirex97@gmail.com>
2019-10-31 13:17:28 +0200

    Add Handle to Fridge and Pass-Through Cabinet.
    Testing out handle animation and how it works in VR.
    (Test hand controls currently cant grab doors).

--- Assets/Scripts/Interactables/DoorHandle.cs
+++ Assets/Scripts/Interactables/DoorHandle.cs
@@ -1,20 +1,81 @@
-﻿public class DoorHandle : Interactable {
+﻿using UnityEngine;
+public class DoorHandle : Interactable {
 
-    #region fields
+    #region Fields
     private Hand hand;
     private OpenableDoor door;
+    #endregion
 
+    #region Animation Fields
+
+    public enum HandleState { Up, Down, Opening, Closing };
+    private HandleState state;
+    private float startAngle;
+    private float currentAngle = 0;
+    private readonly float maxAngle = 45f;
+    private float handleSpeed = 5.5f;
+    private Transform handle;
     #endregion
 
     protected override void Start() {
         base.Start();
         door = transform.parent.GetComponent<OpenableDoor>();
         Type.Set(InteractableType.Interactable);
+        handle = transform.GetChild(1).transform;
+        if (handle != null) {
+            startAngle = handle.eulerAngles.z;
+        }
     }
 
     private void Update() {
         if (State == InteractState.Grabbed) {
             door.SetByHandPosition(hand);
+        }
+        UpdateHandle();
+
+    }
+
+    private void UpdateHandle() {
+        UpdateHandleAnimation();
+        CheckHandleState();
+        if (handle != null) {
+            handle.eulerAngles = new Vector3(handle.eulerAngles.x, handle.eulerAngles.y, startAngle - currentAngle);
+        }
+        PrintInfo();
+
+    }
+
+    private void PrintInfo() {
+        Logger.Print("Current State is:" + state + "\nCurrent Angle:" + startAngle);
+    }
+
+    private void CheckHandleState() {
+        if (hand == null) {
+            if (currentAngle <= 0) {
+                state = HandleState.Up;
+                currentAngle = 0;
+            } else {
+                state = HandleState.Closing;
+            }
+        } else {
+            if (currentAngle >= maxAngle) {
+                state = HandleState.Down;
+                currentAngle = maxAngle;
+            } else {
+                state = HandleState.Opening;
+            }
+        }
+    }
+    private void UpdateHandleAnimation() {
+        switch (state) {
+            case HandleState.Closing:
+                currentAngle -= handleSpeed * Time.deltaTime;
+                break;
+            case HandleState.Opening:
+                currentAngle += handleSpeed * Time.deltaTime;
+                break;
+            default:
+                break;
         }
     }
 
