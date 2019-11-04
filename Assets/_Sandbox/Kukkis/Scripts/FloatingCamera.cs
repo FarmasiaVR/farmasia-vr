d628e81958ea0378f31cd02748bb2e5d9a95d01c
porrasm <31691452+porrasm@users.noreply.github.com>
2019-11-04 16:16:01 +0200

    Interactable rigidbody accessing is now done through Interactable.Rigidbody property

--- Assets/_Sandbox/Kukkis/Scripts/FloatingCamera.cs
+++ Assets/_Sandbox/Kukkis/Scripts/FloatingCamera.cs
@@ -19,7 +19,7 @@ public class FloatingCamera : MonoBehaviour {
     };
     // Start is called before the first frame update
     void Start() {
-        body = transform.GetComponent<Rigidbody>();
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
