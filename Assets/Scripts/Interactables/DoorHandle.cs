FarmasiaVR/farmasia-vr

commit 801e2ae3f0912ae3796293ef05680ec53f3f9ed8

Daniel: Refactor override methods in Interactable

Assets/Scripts/Interactables/DoorHandle.cs
@@ -17,8 +17,7 @@ public class DoorHandle : Interactable {
     private Transform handle;
     #endregion
 
-    protected override void Start() {
-        base.Start();
+    protected override void Start_Impl() {
         door = transform.parent.GetComponent<OpenableDoor>();
         Type.Set(InteractableType.Interactable);
         handle = transform.GetChild(1).transform;

Assets/Scripts/Interactables/DragAcceptable.cs
@@ -27,10 +27,10 @@ public abstract class DragAcceptable : Interactable {
     }
     #endregion
 
-    protected override void Start() {
-        base.Start();
+    protected override void Start_Impl() {
         Initialize();
     }
+
     public void Initialize() {
         mass = Rigidbody.mass;
         Type.On(InteractableType.Interactable, InteractableType.Draggable);

Assets/Scripts/Interactables/Grabbable.cs
@@ -4,8 +4,7 @@
         throw new System.NotImplementedException();
     }
 
-    protected override void Start() {
-        base.Start();
+    protected override void Start_Impl() {
         Type.On(InteractableType.Grabbable);
     }
 }

Assets/Scripts/Interactables/Interactable.cs
@@ -16,7 +16,7 @@ public class Interactable : MonoBehaviour {
             if (RB.Enabled) {
                 return RB.Rigidbody;
             } else {
-                Logger.Print("Accessing rigidbody while disabled");
+                Logger.Warning("Accessing rigidbody while disabled");
                 return null;
             }
         }
@@ -26,26 +26,26 @@ public class Interactable : MonoBehaviour {
     public Interactors Interactors;
     #endregion
 
-    protected virtual void Awake() {
+    private void Awake() {
         RB = new RigidbodyContainer(this);
+        Awake_Impl();
     }
 
-    protected virtual void Start() {
+    private void Start() {
         gameObject.AddComponent<ObjectHighlight>();
         gameObject.AddComponent<ItemPlacement>();
-
         gameObject.tag = iTag;
+        Start_Impl();
     }
 
-    public virtual void Interact(Hand hand) {
-    }
-    public virtual void Interacting(Hand hand) {
-    }
-    public virtual void Uninteract(Hand hand) {
-    }
+    protected virtual void Awake_Impl() {}
+    protected virtual void Start_Impl() {}
+    
 
-    public virtual void UpdateInteract(Hand hand) {
-    }
+    public virtual void Interact(Hand hand) {}
+    public virtual void Interacting(Hand hand) {}
+    public virtual void Uninteract(Hand hand) {}
+    public virtual void UpdateInteract(Hand hand) {}
 
     public static Interactable GetInteractable(Transform t) {
         return GetInteractableObject(t)?.GetComponent<Interactable>();

Assets/Scripts/Interactables/LockedDoorHandle.cs
@@ -6,8 +6,7 @@ public class LockedDoorHandle : Interactable {
 
     #endregion
 
-    protected override void Start() {
-        base.Start();
+    protected override void Start_Impl() {
         door = transform.parent.GetComponent<LockedDoor>();
         Type.Set(InteractableType.Interactable);
     }
@@ -16,9 +15,6 @@ public class LockedDoorHandle : Interactable {
         if (State == InteractState.Grabbed) {
             door.CheckExitPermission();
         }
-    }
-
-    private void UpdatePosition() {
     }
 
     public override void Interact(Hand hand) {

Assets/Scripts/Interactables/RoomDoorHandle.cs
@@ -8,8 +8,7 @@ public class RoomDoorHandle : Interactable {
     private DoorGoTo destination;
     #endregion
 
-    protected override void Start() {
-        base.Start();
+    protected override void Start_Impl() {
         Type.Set(InteractableType.Interactable);
     }
 

Assets/Scripts/Objects/Equipment/LuerlockAdapter.cs
@@ -42,9 +42,7 @@ public class LuerlockAdapter : GeneralItem {
     public bool HasAttachedObjects { get => ObjectCount > 0; }
     #endregion
 
-    protected override void Start() {
-        base.Start();
-
+    protected override void Start_Impl() {
         ObjectType = ObjectType.Luerlock;
         Type.On(InteractableType.SmallObject);
 

Assets/Scripts/Objects/Equipment/MedicineBottle.cs
@@ -7,8 +7,7 @@ public class MedicineBottle : GeneralItem {
     public LiquidContainer Container { get; private set; } 
     #endregion
 
-    protected override void Start() {
-        base.Start();
+    protected override void Start_Impl() {
         Container = LiquidContainer.FindLiquidContainer(transform);
         Assert.IsNotNull(Container);
         ObjectType = ObjectType.Bottle;

commit c4e4c8f7d7438adaf992741bf972b12a8dfddb0a

Fix grab bug introduced in 801e2ae

Assets/Scripts/Interactables/DoorHandle.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+
 public class DoorHandle : Interactable {
 
     #region Fields
@@ -17,7 +18,7 @@ public class DoorHandle : Interactable {
     private Transform handle;
     #endregion
 
-    protected override void Start_Impl() {
+    protected override void Start_Interactable() {
         door = transform.parent.GetComponent<OpenableDoor>();
         Type.Set(InteractableType.Interactable);
         handle = transform.GetChild(1).transform;

Assets/Scripts/Interactables/DragAcceptable.cs
@@ -27,9 +27,17 @@ public abstract class DragAcceptable : Interactable {
     }
     #endregion
 
-    protected override void Start_Impl() {
-        Initialize();
+    protected override void Awake_Interactable() {
+        Awake_DragAcceptable();
     }
+
+    protected override void Start_Interactable() {
+        Initialize();
+        Start_DragAcceptable();
+    }
+
+    protected virtual void Awake_DragAcceptable() {}
+    protected virtual void Start_DragAcceptable() {}
 
     public void Initialize() {
         mass = Rigidbody.mass;

Assets/Scripts/Interactables/Grabbable.cs
@@ -4,7 +4,15 @@
         throw new System.NotImplementedException();
     }
 
-    protected override void Start_Impl() {
-        Type.On(InteractableType.Grabbable);
+    protected override void Awake_Interactable() {
+        Awake_Grabbable();
     }
+
+    protected override void Start_Interactable() {
+        Type.On(InteractableType.Grabbable);
+        Start_Grabbable();
+    }
+
+    protected virtual void Awake_Grabbable() {}
+    protected virtual void Start_Grabbable() {}
 }

Assets/Scripts/Interactables/Interactable.cs
@@ -28,18 +28,18 @@ public class Interactable : MonoBehaviour {
 
     private void Awake() {
         RB = new RigidbodyContainer(this);
-        Awake_Impl();
+        Awake_Interactable();
     }
 
     private void Start() {
         gameObject.AddComponent<ObjectHighlight>();
         gameObject.AddComponent<ItemPlacement>();
         gameObject.tag = iTag;
-        Start_Impl();
+        Start_Interactable();
     }
 
-    protected virtual void Awake_Impl() {}
-    protected virtual void Start_Impl() {}
+    protected virtual void Awake_Interactable() {}
+    protected virtual void Start_Interactable() {}
     
 
     public virtual void Interact(Hand hand) {}

Assets/Scripts/Interactables/LockedDoorHandle.cs
@@ -6,7 +6,7 @@ public class LockedDoorHandle : Interactable {
 
     #endregion
 
-    protected override void Start_Impl() {
+    protected override void Start_Interactable() {
         door = transform.parent.GetComponent<LockedDoor>();
         Type.Set(InteractableType.Interactable);
     }

Assets/Scripts/Interactables/RoomDoorHandle.cs
@@ -8,7 +8,7 @@ public class RoomDoorHandle : Interactable {
     private DoorGoTo destination;
     #endregion
 
-    protected override void Start_Impl() {
+    protected override void Start_Interactable() {
         Type.Set(InteractableType.Interactable);
     }
 

Assets/Scripts/Objects/Equipment/LuerlockAdapter.cs
@@ -42,7 +42,7 @@ public class LuerlockAdapter : GeneralItem {
     public bool HasAttachedObjects { get => ObjectCount > 0; }
     #endregion
 
-    protected override void Start_Impl() {
+    protected override void Start_GeneralItem() {
         ObjectType = ObjectType.Luerlock;
         Type.On(InteractableType.SmallObject);
 

Assets/Scripts/Objects/Equipment/MedicineBottle.cs
@@ -7,7 +7,7 @@ public class MedicineBottle : GeneralItem {
     public LiquidContainer Container { get; private set; } 
     #endregion
 
-    protected override void Start_Impl() {
+    protected override void Start_GeneralItem() {
         Container = LiquidContainer.FindLiquidContainer(transform);
         Assert.IsNotNull(Container);
         ObjectType = ObjectType.Bottle;

Assets/Scripts/Objects/Equipment/Syringe.cs
@@ -24,7 +24,7 @@ public class Syringe : GeneralItem {
     // private Pipeline pipeline = new Pipeline();
 
     #endregion
-    protected override void Start_Impl() {
+    protected override void Start_GeneralItem() {
         Container = LiquidContainer.FindLiquidContainer(transform);
         Assert.IsNotNull(Container);
         ObjectType = ObjectType.Syringe;

Assets/Scripts/Objects/Equipment/TestEquipment.cs
@@ -10,7 +10,7 @@ public class TestEquipment : GeneralItem {
     [SerializeField]
     private LiquidContainer dest;
 
-    protected override void Start_Impl() {
+    protected override void Start_GeneralItem() {
         Assert.IsNotNull(src);
     }
 

Assets/Scripts/Objects/GeneralItem.cs
@@ -16,6 +16,17 @@ public class GeneralItem : Grabbable {
         return Interactable.GetInteractableObject(t)?.GetComponent<GeneralItem>();
     }
 
+    protected override void Awake_Grabbable() {
+        Awake_GeneralItem();
+    }
+
+    protected override void Start_Grabbable() {
+        Start_GeneralItem();
+    }
+
+    protected virtual void Awake_GeneralItem() {}
+    protected virtual void Start_GeneralItem() {}
+
     protected virtual void OnCollisionEnter(Collision coll) {
         if (coll.gameObject.tag == "Floor") IsClean = false;
     }

Assets/Scripts/Objects/HintBox.cs
@@ -44,12 +44,12 @@ public class HintBox : DragAcceptable {
         }
     }
 
-    private void Awake() {
+    protected override void Awake_DragAcceptable() {
         targetSize = transform.localScale;
         transform.localScale = Vector3.zero;
     }
 
-    protected override void Start_Impl() {
+    protected override void Start_Interactable() {
         Type.On(InteractableType.Interactable, InteractableType.Draggable);
 
         playerCamera = Player.Camera.transform;

Assets/Scripts/RoomFunctions/LaminarCabinetButton.cs
@@ -1,6 +1,6 @@
 public class LaminarCabinetButton : Interactable {
 
-    protected override void Start_Impl() {
+    protected override void Start_Interactable() {
         Type.Set(InteractableType.Interactable);
     }
 