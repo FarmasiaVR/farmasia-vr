c908951031325a3a5410ddf38d3d485f13ba9060
Farmasia VR <farmasia.vr@gmail.com>
2019-11-08 13:02:01 +0200

    Daniel: Refactor and fix RoomTeleport.cs

--- Assets/Scripts/RoomFunctions/RoomTeleport.cs
+++ Assets/Scripts/RoomFunctions/RoomTeleport.cs
@@ -1,38 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class RoomTeleport : MonoBehaviour {
     #region Fields
-    [Tooltip("Insert Teleportation Spot for the player here!")]
+    [Tooltip("VRPlayer instance")]
     [SerializeField]
-    public GameObject playerTele;
-    [Tooltip("Insert Pass-Through cabinet teleportation here!")]
+    private Transform player;
+    [Tooltip("Player teleportation destination")]
     [SerializeField]
-    public GameObject passThroughTele;
-    [SerializeField]
-    public GameObject passThroughTriggerLocal;
-    private CabinetBase cabinet;
-    private GameObject player;
-    #endregion
+    public Transform playerDst;
 
-    private void Start() {
-        cabinet = GameObject.FindGameObjectWithTag("PassThrough (Prep)")?.GetComponent<CabinetBase>();
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+    [Tooltip("Passthrough teleportation source")]
+    [SerializeField]
+    public Transform passthroughSrc;
+    [Tooltip("Passthrough teleportation destination")]
+    [SerializeField]
+    public Transform passthroughDst;
+    #endregion
 
     /// <summary>
     /// Teleports player and Contents of Pass-Through cabinet to the next room.
     /// </summary>
     public void TeleportPlayerAndPassthroughCabinet() {
-        if (playerTele == null || passThroughTele == null) {
+        if (playerDst == null || passthroughDst == null) {
             Logger.Print("Cannot teleport without references to teleportation spots!");
             return;
         }
-        foreach (GameObject cabinetItem in cabinet.GetContainedItems()) {
-            Vector3 newPosition = passThroughTele.transform.position + (cabinetItem.transform.position - passThroughTriggerLocal.transform.position);
-            float rotDelta = Quaternion.Angle(passThroughTriggerLocal.transform.rotation, passThroughTele.transform.rotation);
-            cabinetItem.transform.position = newPosition;
-            cabinetItem.transform.RotateAround(passThroughTele.transform.position, passThroughTele.transform.up, rotDelta);
+
+        CabinetBase cabinet = passthroughSrc.GetComponent<CabinetBase>();
+        List<Transform> items = cabinet.GetContainedItems().ConvertAll(obj => obj.transform);
+        foreach (Transform item in items) {
+            float rotDelta = Quaternion.Angle(passthroughSrc.rotation, passthroughDst.rotation);
+            item.position = passthroughDst.position + (item.position - passthroughSrc.position);
+            item.RotateAround(passthroughDst.position, passthroughDst.up, rotDelta);
         }
-        player.transform.position = new Vector3(playerTele.transform.position.x, playerTele.transform.position.y, playerTele.transform.position.z);
+        player.position = playerDst.position;// new Vector3(playerDst.position.x, playerDst.position.y, playerDst.position.z);
     }
 }
