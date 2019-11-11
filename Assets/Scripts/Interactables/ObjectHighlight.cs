FarmasiaVR/farmasia-vr

commit 39e4578f46bdcd4a84ac08db1efd0edade083369

Daniel: Fix rendered null reference exception in ObjectHighlight

Assets/Scripts/Interactables/ObjectHighlight.cs
@@ -12,8 +12,8 @@ public class ObjectHighlight : MonoBehaviour {
 
     private void Awake() {
         InitializeLists();
-        highlightColor = new Color32(100,120,100,1);
-        normalColor = new Color32(0,0,0,0);
+        highlightColor = new Color32(100, 120, 100, 1);
+        normalColor = new Color32(0, 0, 0, 0);
     }
 
     private void OnDestroy() {
@@ -38,20 +38,16 @@ public class ObjectHighlight : MonoBehaviour {
 
     private void InitializeLists() {
         materials = new List<Material>();
-        AddAllChildren(transform, materials);
 
-        void AddAllChildren(Transform c, List<Material> materials) {
-            AddObjectMaterial(c.GetComponent<Renderer>()?.material, materials);
+        Renderer r = GetComponent<Renderer>();
 
-            foreach (Transform t in c) {
-                AddAllChildren(t, materials);
-            }
+        if (r != null && r.material != null) {
+            materials.Add(r.material);
         }
 
-        void AddObjectMaterial(Material mat, List<Material> materials) {
-            if (mat != null) {
-                mat.EnableKeyword("_EMISSION");
-                materials.Add(mat);
+        foreach (Renderer rc in transform.GetComponentsInChildren<Renderer>()) {
+            if (rc.material != null) {
+                materials.Add(rc.material);
             }
         }
     }

commit c0809e79439eeb5ef05be4c9aaf1458f42aa06ce

Daniel: Fix bug introduced in 39e4578

Assets/Scripts/Interactables/ObjectHighlight.cs
@@ -42,12 +42,14 @@ public class ObjectHighlight : MonoBehaviour {
         Renderer r = GetComponent<Renderer>();
 
         if (r != null && r.material != null) {
+            r.material.EnableKeyword("_EMISSION");
             materials.Add(r.material);
         }
 
-        foreach (Renderer rc in transform.GetComponentsInChildren<Renderer>()) {
-            if (rc.material != null) {
-                materials.Add(rc.material);
+        foreach (Renderer child in transform.GetComponentsInChildren<Renderer>()) {
+            if (child.material != null) {
+                child.material.EnableKeyword("_EMISSION");
+                materials.Add(child.material);
             }
         }
     }