2965b2e5efc86c055cc106ec472d4fab3c33bb1d
Mirex97 <Mirex97@gmail.com>
2019-11-13 18:03:20 +0200

    Add Needle class.
    Fixed Needle prefab to have Needle class instead of GeneralItem.
    Added Interactable tag to Needle objects.

---
+++ Assets/Scripts/Objects/Equipment/Needle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needle : GeneralItem {
+    protected override void Start() {
+        ObjectType = ObjectType.Needle;
+    }
+}
