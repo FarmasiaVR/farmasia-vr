3ebde8c4207044eeb544e10d646e4f8824e3678e
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-11-18 14:15:27 +0200

    Make RoomDoorHandle an AnimatedDoorHandle

--- Assets/Scripts/Interactables/RoomDoorHandle.cs
+++ Assets/Scripts/Interactables/RoomDoorHandle.cs
@@ -1,9 +1,9 @@
 using System;
 using UnityEngine;
 
-public class RoomDoorHandle : Interactable {
+public class RoomDoorHandle : AnimatedDoorHandle {
 
-    #region field
+    #region Fields
     [SerializeField]
     private DoorGoTo destination;
     #endregion
