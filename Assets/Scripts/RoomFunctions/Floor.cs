05ccc817f94c56086f4c382b717f9f3ad854e220
Mirex <Mirex97@gmail.com>
2019-11-18 13:46:33 +0200

    Floor.cs update to use Trigger instead of collision.

--- Assets/Scripts/RoomFunctions/Floor.cs
+++ Assets/Scripts/RoomFunctions/Floor.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour {
 
-    private void OnCollisionEnter(Collision col) {
+    private void OnTriggerEnter(Collider col) {
         GeneralItem item = GeneralItem.Find(col.gameObject.transform);
         if (item != null) {
             Events.FireEvent(EventType.ItemDroppedOnFloor, CallbackData.Object(item));
