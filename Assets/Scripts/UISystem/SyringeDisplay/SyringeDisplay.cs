c743e28c83ff7bda842afe0c702dac6cd407403a
Mirex97 <Mirex97@gmail.com>
2019-11-28 19:22:16 +0200

    Add SyringeDisplay.
    Added SyringeDisplayer object which spawns LiquidDisplay to grabbed
    Syringes.
    LiquidDisplay is a separate object that follows syringes and removes
    itself if player is not grabbing the given syringe anymore.

---
+++ Assets/Scripts/UISystem/SyringeDisplay/SyringeDisplay.cs
@@ -0,0 +1,48 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SyringeDisplay : MonoBehaviour {
+
+    #region Fields
+    [SerializeField]
+    GameObject textObject;
+    GameObject followedObject;
+    Transform textParentTransform;
+    GameObject cam;
+    Syringe syringe;
+    LiquidContainer container;
+    TextMeshPro textField;
+    bool liquidPresent = false;
+    #endregion
+
+    #region Constants
+    private const string VOLUME = "ml";
+    #endregion
+
+    public void setFollowedObject(GameObject follow) {
+        followedObject = follow;
+        syringe = followedObject.GetComponent<Syringe>();
+        container = syringe.Container;
+        liquidPresent = true;
+    }
+
+    void Start() {
+        textParentTransform = textObject.transform.parent.transform;
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        textField = textObject.GetComponent<TextMeshPro>();
+    }
+
+    void Update() {
+        //transform.LookAt(cam.transform, Vector3.up);
+        if (followedObject != null) {
+            transform.position = followedObject.transform.position;
+        } else {
+            Logger.Warning("No object to follow in SyringeDisplay, Destroying!");
+            Destroy(gameObject);
+        }
+        textParentTransform.LookAt(cam.transform, Vector3.up);
+        if (liquidPresent) {
+            textField.text = container.Amount + "/" + container.Capacity + VOLUME;
+        }
+    }
+}
