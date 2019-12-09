04251e2402f66f410d3b79a2ec18f747f44491a2
Hassinen Heidi K <heidihas@sf-12.cs.helsinki.fi>
2019-12-09 16:06:39 +0200

    InfoBox

--- Assets/Scripts/Objects/InfoBox.cs
+++ Assets/Scripts/Objects/InfoBox.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InfoBox : MonoBehaviour {
@@ -13,11 +15,15 @@ public class InfoBox : MonoBehaviour {
     private string message;
     private bool preparationMessageShown = false;
     private bool workspaceMessageShown = false;
+    TextMeshPro textField;
+    GameObject cam;
     #endregion
 
     private void Awake() {
         message = PREPARATION_ROOM_MESSAGE;
         Subscribe();
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        textField = this.GetComponent<TextMeshPro>();
     }
 
     #region Event Subscriptions
@@ -48,9 +54,23 @@ public class InfoBox : MonoBehaviour {
     }
 
     private void ShowInfoBox() {
+        transform.position = cam.transform.position;
+        textField.text = message;
+
+        //Pipeline pipe = G.Instance.Pipeline.New().Delay(100);
+        var date1 = DateTime.Now;
+        var date2 = DateTime.Now;
+        Logger.Print("aluksi: " + date1);
+
+        while(Convert.ToSingle((date2-date1).TotalSeconds) < 15) {
+            date2 = DateTime.Now;
+            //float seconds = Convert.ToSingle((date2 - date1).TotalSeconds);
+            //date1 = date2;
+            Logger.Print(date2);
+            //G.Instance.Pipeline.Update(seconds);
+        }
+
+        textField.text = "";
     }
     #endregion
-
-    protected void Activate() {
-    }
 }
