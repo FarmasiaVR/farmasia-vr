191f1e4ec560b9760d325c71e93df51c0d76d590
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-12-09 16:10:27 +0200

    Remove unused code

--- Assets/Scripts/UISystem/SyringeDisplay/SyringeDisplay.cs
+++ Assets/Scripts/UISystem/SyringeDisplay/SyringeDisplay.cs
@@ -33,7 +33,6 @@ public class SyringeDisplay : MonoBehaviour {
     }
 
     void Update() {
-        //transform.LookAt(cam.transform, Vector3.up);
         if (followedObject != null) {
             transform.position = followedObject.transform.position;
         } else {
