7bc274aa6844f7c7700ca5125fb0a8cf94a6721c
Mirex <Mirex97@gmail.com>
2019-12-10 13:11:16 +0200

    MenuInterface versio 1.0
    Testing does it work or not.
    Missing Query for "Are you sure?".

---
+++ Assets/Scripts/MainMenu/MenuButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MenuButton : Interactable {
+    [SerializeField]
+    private MenuInterface menu;
+
+    [SerializeField]
+    private SceneTypes scene;
+
+    [SerializeField]
+    private bool isCloseButton = false;
+
+    private SceneLoader changer;
+
+    protected override void Start() {
+        base.Start();
+        Type.Set(InteractableType.Interactable);
+        changer = GameObject.FindGameObjectWithTag("LevelChanger").GetComponent<SceneLoader>();
+    }
+    public override void Interact(Hand hand) {
+        base.Interact(hand);
+        if (isCloseButton) {
+            menu.Close();
+        }
+        changer.SwapScene(scene);
+    }
+
+    public override void Uninteract(Hand hand) {
+        base.Uninteract(hand);
+    }
+}
---
+++ Assets/Scripts/MainMenu/MenuInterface.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class MenuInterface : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject menuContainer;
+
+
+
+    private Hand hand;
+    private VRPadClick click;
+
+    public bool Visible => menuContainer.activeSelf;
+
+    public void Close() {
+        menuContainer.SetActive(!Visible);
+    }
+
+    private void Start() {
+        hand = GameObject.FindGameObjectWithTag("ControllerLeft")?.GetComponent<Hand>();
+    }
+
+    private void Update() {
+        if (hand != null && VRInput.GetControlDown(hand.HandType, ControlType.Menu)) {
+            Close();
+        }
+    }
+}
