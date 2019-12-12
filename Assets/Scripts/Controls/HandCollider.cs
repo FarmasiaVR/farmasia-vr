693a46c3c23b0f6287348f25b1128c57782a1c2d
farmasia-vr <55394182+farmasia-vr@users.noreply.github.com>
2019-12-12 16:51:31 +0200

    Eetu: Add distinction between Vive and Index controllers -> different colliders

--- Assets/Scripts/Controls/HandCollider.cs
+++ Assets/Scripts/Controls/HandCollider.cs
@@ -1,15 +1,64 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class HandCollider : MonoBehaviour {
 
     #region Fields
     private HashSet<Interactable> grabObjects;
     public ObjectHighlight PreviousHighlight { get; private set; }
+
+    private enum ModelType {
+        None = -1,
+        Vive = 2,
+        Index = 4
+    }
+
+    private static Vector3 INDEX_POS = new Vector3(0, 0, -0.0863f);
+    private static Vector3 VIVE_POS = new Vector3(0, -0.03f, 0.015f);
     #endregion
 
     private void Start() {
         grabObjects = new HashSet<Interactable>();
+
+        StartCoroutine(SetCollPos());
+    }
+
+    private IEnumerator SetCollPos() {
+
+        while (true) {
+            var type = GetModelType();
+
+            yield return null;
+
+            if (type == SteamVR_TrackedObject.EIndex.None) {
+                continue;
+            }
+
+            if (BIsHTCViveController(type)) {
+                Logger.Print("Using VIVE controller");
+                transform.localPosition = VIVE_POS;
+            } else {
+                Logger.Print("Using INDEX controller");
+                transform.localPosition = INDEX_POS;
+            }
+
+            break;
+        }
+    }
+
+    bool BIsHTCViveController(SteamVR_TrackedObject.EIndex type) {
+        System.Text.StringBuilder sbType = new System.Text.StringBuilder(1000);
+        Valve.VR.ETrackedPropertyError err = Valve.VR.ETrackedPropertyError.TrackedProp_Success;
+        SteamVR.instance.hmd.GetStringTrackedDeviceProperty((uint)type, Valve.VR.ETrackedDeviceProperty.Prop_ManufacturerName_String, sbType, 1000, ref err);
+        return (err == Valve.VR.ETrackedPropertyError.TrackedProp_Success && sbType.ToString().StartsWith("HTC"));
+    }
+
+    private SteamVR_TrackedObject.EIndex GetModelType() {
+        Transform model = transform.parent.Find("Model");
+        SteamVR_RenderModel m = model.GetComponent<SteamVR_RenderModel>();
+        return m.index;
     }
 
     private void OnTriggerEnter(Collider coll) {
@@ -90,6 +139,13 @@ public class HandCollider : MonoBehaviour {
         Interactable closest = null;
 
         foreach (Interactable g in grabObjects) {
+        grabObjects.Remove(null);
+
+            if (g == null) {
+                Logger.Print("G was null");
+            } else if (g.transform == null) {
+                Logger.Print("G.Transform was null");
+            }
             float angle = Vector3.Angle(transform.forward, g.transform.position - transform.position);
 
             if (angle < smallestAngle && angle < maxAngle) {
