b099b438f936fc01fa7276b5562707c134c8eaf3
Mirex97 <Mirex97@gmail.com>
2020-01-08 15:52:57 +0200

    Floating Interface

--- Assets/Scripts/MainMenu/MenuInterface.cs
+++ Assets/Scripts/MainMenu/MenuInterface.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 
-
 public class MenuInterface : MonoBehaviour {
 
     [SerializeField]
     private GameObject menuContainer;
     private Hand hand;
+    [SerializeField]
+    private Transform camera;
+    [SerializeField]
+    private Vector3 offset;
+
+    [SerializeField]
+    private float lerpAmount;
 
     public bool Visible => menuContainer.activeSelf;
 
@@ -14,14 +20,25 @@ public class MenuInterface : MonoBehaviour {
     }
 
     private void Start() {
+        if (camera == null) {
+            camera = GameObject.FindGameObjectWithTag("MainCamera")?.transform;
+        }
         hand = GameObject.FindGameObjectWithTag("Controller (Left)")?.GetComponent<Hand>();
     }
 
     private void Update() {
+        if (Visible) {
+            transform.LookAt(camera, Vector3.up);
+            transform.position = Vector3.Lerp(transform.position, GetTargetPosition(), Time.deltaTime / lerpAmount);
+        }
         if (hand != null && VRInput.GetControlDown(hand.HandType, Controls.Menu)) {
             Close();
         } else if (hand == null) {
             Logger.Warning("Hand is Null!");
         }
+    }
+
+    private Vector3 GetTargetPosition() {
+        return camera.TransformPoint(offset);
     }
 }
