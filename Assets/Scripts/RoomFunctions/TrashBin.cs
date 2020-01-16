28d845578df2a58f188a08b5ee830a6cc8273f20
SirVeggie <sirveggie@hotmail.com>
2020-01-16 20:13:35 +0200

    Trash modifications

--- Assets/Scripts/RoomFunctions/TrashBin.cs
+++ Assets/Scripts/RoomFunctions/TrashBin.cs
@@ -3,45 +3,21 @@ using UnityEngine;
 public class TrashBin : MonoBehaviour {
 
     #region fields
-    //[SerializeField]
-    //protected GameObject childCollider;
     [SerializeField]
     private bool sharpTrash;
     #endregion
-
-    //private void Start() {
-        //Setup(false);
-    //}
-    
-    //protected void Setup(bool checkForSharp) {
-        //CollisionSubscription.SubscribeToTrigger(childCollider, new TriggerListener().OnEnter(collider => EnterTrashbin(collider, checkForSharp)));
-    //}
-
-    //private void EnterTrashbin(Collider other, bool checkForSharp) {
-        //GeneralItem item = GeneralItem.Find(other.transform);
-        //if (item != null) {
-            //if (!checkForSharp || item.ObjectType == ObjectType.Needle) {
-                //Events.FireEvent(EventType.ItemDroppedInTrash, CallbackData.Object(item));
-                //item.DestroyInteractable();
-            //} else {
-                //Logger.Print("Non sharp item placed in trash");
-                //Logger.Print(item.ObjectType);
-            //}
-        //}
-    //}
 
     public void EnterTrashbin(Collider other) {
         GeneralItem item = GeneralItem.Find(other.transform);
         
         if (item != null) {
-            if (sharpTrash == (item.ObjectType == ObjectType.Needle)) {
-                Events.FireEvent(EventType.ItemDroppedInTrash, CallbackData.Object(item));
+            bool isNeedle = item.ObjectType == ObjectType.Needle;
+            if (sharpTrash == isNeedle) {
+                //Events.FireEvent(EventType.ItemDroppedInTrash, CallbackData.Object(item));
             } else {
-                Logger.Warning("Item placed in the wrong trash");
-                Logger.Print(item.ObjectType);
+                Logger.Warning("Item placed in the wrong trash: " + item.ObjectType);
             }
 
-            Logger.Print("Destroying: " + item);
             item.DestroyInteractable();
         }
     }
--- Assets/Scripts/RoomFunctions/TrashBinCollider.cs
+++ Assets/Scripts/RoomFunctions/TrashBinCollider.cs
@@ -12,7 +12,7 @@ public class TrashBinCollider : MonoBehaviour {
         mainScript = transform.GetComponentInParent<TrashBin>();
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerStay(Collider other) {
         mainScript.EnterTrashbin(other);
     }
 }
