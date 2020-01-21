66f88d1873ac87cb8582f8d356323c8e2a49fd86
farmasia-vr <55394182+farmasia-vr@users.noreply.github.com>
2020-01-21 16:58:21 +0200

    Eetu: LaminarCabinetTable now contaminates objects

---
+++ Assets/Scripts/TriggerInteractableContainer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerInteractableContainer : MonoBehaviour {
+
+    private HashSet<Interactable> interactables;
+
+    private void Awake() {
+        interactables = new HashSet<Interactable>();
+    }
+
+    public bool Contains(Interactable interactable) {
+        return interactables.Contains(interactable);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (Interactable.GetInteractable(other.transform) is var i && i != null) {
+            interactables.Add(i);
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (Interactable.GetInteractable(other.transform) is var i && i != null) {
+            interactables.Remove(i);
+        }
+    }
+}
