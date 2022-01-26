5bc4f1e6491164f25baf3520bca764e978b5feaf
vrfarmasia <vrfarmasia@gmail.com>
2022-01-26 13:19:04 +0200

    Added static code analysis by Project Auditor

---
+++ Assets/Scripts/ProjectAuditor/ProjectAuditorCI.cs
@@ -0,0 +1,13 @@
+﻿using Unity.ProjectAuditor.Editor;
+using UnityEngine;
+
+public static class ProjectAuditorCI {
+    public static void AuditAndExport() {
+        var projectAuditor = new Unity.ProjectAuditor.Editor.ProjectAuditor();
+        var projectReport = projectAuditor.Audit();
+
+        var codeIssues = projectReport.GetIssues(IssueCategory.Code);
+
+        Debug.Log("Project Auditor found " + codeIssues.Length + " code issues");
+    }
+}
