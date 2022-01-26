9f8d39910f21baf5691b8c4966a7389a49c59433
vrfarmasia <vrfarmasia@gmail.com>
2022-01-26 17:25:53 +0200

    Add pipette to preparation room

---
+++ Assets/Scripts/Objects/Equipment/Pipette.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipette : GeneralItem {
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnGrabStart(Hand hand) {
+        base.OnGrabStart(hand);
+        if (!IsClean) {
+            Logger.Print("Pipette grabbed and it was dirty!");
+        }
+        
+    }
+}
