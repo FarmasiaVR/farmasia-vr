156e5e586499bca9f4d7a8d22baa0d4349f74546
vrfarmasia <vrfarmasia@gmail.com>
2022-02-01 16:41:01 +0200

    Fixed Auditor errors (?)

--- Assets/Scripts/UISystem/FloatingDisplay/LiquidDisplay.cs
+++ Assets/Scripts/UISystem/FloatingDisplay/LiquidDisplay.cs
@@ -1,12 +1,15 @@
 ﻿using TMPro;
 using UnityEngine;
+using System.Text;
 
 public class LiquidDisplay : Display {
 
     LiquidContainer container;
     TextMeshPro textField;
     bool liquidPresent = false;
-    
+
+    private StringBuilder stringBuilder;
+
     private const string VOLUME = "ml";
 
     public override void SetFollowedObject(GameObject follow) {
@@ -32,15 +35,29 @@ public class LiquidDisplay : Display {
 
     new void Start() {
         base.Start();
+        stringBuilder.EnsureCapacity(16);
+        stringBuilder.Append("12.123/12.123" + VOLUME);
         textField = base.textObject.GetComponent<TextMeshPro>();
     }
 
     new void Update() {
         base.Update();
         if (liquidPresent) {
-            double contAmount = (double)container.Amount / 1000;
-
-            textField.text = contAmount.ToString("F3") + "/" + ((double)container.Capacity / 1000) + VOLUME;
+            UpdateText();
         }
+    }
+
+    private void UpdateText() {
+        stringBuilder.Clear();
+
+        double contAmount = 1.0 * container.Amount / 1000;
+        stringBuilder.Append(contAmount.ToString("F3"));
+        stringBuilder.Append("/");
+
+        double contCapacity = 1.0 * container.Capacity / 1000;
+        stringBuilder.Append(contCapacity.ToString("F3"));
+        stringBuilder.Append(VOLUME);
+
+        textField.SetText(stringBuilder.ToString());
     }
 }
