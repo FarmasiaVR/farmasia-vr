b462e32f067d7cfed8935ee2109dada05868ae2d
vrfarmasia <vrfarmasia@gmail.com>
2022-02-02 16:38:01 +0200

    Started working on bottle connection

---
+++ Assets/Scripts/Objects/Equipment/BottleCap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottleCap : GeneralItem {
+
+    public BottleCapConnector Connector { get; private set; }
+
+    protected override void Start() {
+        base.Start();
+        ObjectType = ObjectType.Needle;
+        Type.On(InteractableType.Interactable, InteractableType.SmallObject);
+
+        Connector = new BottleCapConnector(this, transform.Find("Bottle Opening").gameObject);
+        Connector.Subscribe();
+    }
+
+    public void ReleaseItem() {
+        Connector.Connection?.Remove();
+    }
+}
