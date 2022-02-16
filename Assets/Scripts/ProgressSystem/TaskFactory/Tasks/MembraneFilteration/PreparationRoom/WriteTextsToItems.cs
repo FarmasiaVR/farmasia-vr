f25d8b4a1a9b94ce3b49f5dc4e07124c9468e2f3
dev-viikki <vrfarmasia@gmail.com>
2022-02-16 13:52:25 +0200

    started WriteTextsToItems class for progress system -santeri

---
+++ Assets/Scripts/ProgressSystem/TaskFactory/Tasks/MembraneFilteration/PreparationRoom/WriteTextsToItems.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System;
+public class WriteTextsToItems : TaskBase
+{
+    #region Constants
+    private const string DESCRIPTION = "Kirjoita tarvittavat tiedot pulloihin ja maljoihin";
+    private const string HINT = "Kosketa kynällä esinettä, johon haluat kirjoittaa, valitse kirjoitettavat tekstit (max 4) klikkaamalla niitä. Voit perua kirjoituksen painamalla tekstiä uudestaan ennen kuin painat vihreää nappia";
+    #endregion
+
+    #region Fields
+
+    /// <summary>
+    /// Conditions must be met to render task complete
+    /// </summary>
+    public enum Conditions { SoycaseinePlatesHaveText, SabouradDextrosiPlateHasText, BottlesHaveText }
+    private int numberOfObjectsThatHaveText = 0;
+    private int numberOfObjectsThatShouldHaveText = 8;
+    #endregion
+
+    public WriteTextsToItems() : base(TaskType.WriteTextsToItems, true, false)
+    {
+        Subscribe();
+
+    }
+
+    #region Event Subscriptions
+    public override void Subscribe()
+    {
+        base.SubscribeEvent(DoSomething, EventType.WriteToObject);
+    }
+
+    private void DoSomething(CallbackData data)
+    {
+        GameObject gobj = (GameObject)data.DataObject;
+        ObjectType type = gobj.GetComponent<GeneralItem>().ObjectType;
+        Logger.Print("Progress system object type: " + type);
+        Writable text = gobj.GetComponent<Writable>();
+        Logger.Print("Progress sytemiä varten tekstin löytyminen: " + text.Text);
+    }
+    #endregion
+    protected override void OnTaskComplete()
+    {
+        //////////////////////// TODO /////////////////////////
+        throw new NotImplementedException();
+    }
+
+    #region Public Methods
+
+    public override void FinishTask()
+    {
+        base.FinishTask();
+
+    }
+
+    public override string GetDescription()
+    {
+        return DESCRIPTION;
+    }
+
+    public override string GetHint()
+    {
+        return HINT;
+    }
+    #endregion
+}
