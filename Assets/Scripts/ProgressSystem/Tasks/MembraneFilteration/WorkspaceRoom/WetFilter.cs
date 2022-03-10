48107a3765b77a6fae24b53d35810ab027a8c594
dev-viikki <vrfarmasia@gmail.com>
2022-03-10 14:19:57 +0200

    Wet Filter task created -Santeri

---
+++ Assets/Scripts/ProgressSystem/Tasks/MembraneFilteration/WorkspaceRoom/WetFilter.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class WetFilter : TaskBase {
+
+    public enum Conditions { FilterIsWet }
+
+    private int soycaseineBottlesDone = 0;
+    private int tioglygolateBottlesDone = 0;
+    private PumpFilter pumpFilter;
+
+    private readonly int REQUIRED_AMOUNT = 10000;
+
+    private HashSet<PumpFilter> bottles = new HashSet<PumpFilter>();
+
+    public WetFilter() : base(TaskType.WetFilter, true, false) {
+        SetCheckAll(true);
+        AddConditions((int[])Enum.GetValues(typeof(Conditions)));
+        SubscribeEvent(OnFilterWet, EventType.TransferLiquidToBottle);
+    }
+
+    private void OnFilterWet(CallbackData data) {
+        LiquidContainer container = data.DataObject as LiquidContainer;
+        if (container.GeneralItem is PumpFilter filter && filter.ObjectType == ObjectType.PumpFilter) {
+            pumpFilter = filter;
+            if (filter.Container.Amount >= REQUIRED_AMOUNT) {
+                EnableCondition(Conditions.FilterIsWet);
+                CheckMistakes();
+                CompleteTask();
+            }
+        }
+    }
+
+    private void CheckMistakes() {
+        if (pumpFilter.Container.Amount > REQUIRED_AMOUNT) {
+            CreateTaskMistake("Filtterissä on liikaa nestettä", 1);
+        }
+        if (pumpFilter.Container.Impure) {
+            CreateTaskMistake("Filtterin neste on sekoittunut", 1);
+        }
+    }
+
+    protected override void OnTaskComplete() {
+        // juu
+    }
+
+    public override void CompleteTask() {
+        base.CompleteTask();
+        if (IsCompleted()) {
+            Popup("Hienosti kostutettu!", MsgType.Done);
+        }
+    }
+
+    public override string GetDescription() {
+        return "Kostuta filtteri:D";
+    }
+
+    public override string GetHint() {
+        return "Caman kyl sä osaat";
+    }
+}
