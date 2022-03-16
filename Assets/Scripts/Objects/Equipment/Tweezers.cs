47a799a0165f5d06b6a0c8fe60ee5845fb5725ea
hepitk <heino.pitkanen@helsinki.fi>
2022-03-16 19:52:49 +0200

    Added first version of cover opening

--- Assets/Scripts/Objects/Equipment/Tweezers.cs
+++ Assets/Scripts/Objects/Equipment/Tweezers.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tweezers : GeneralItem
-{
+public class Tweezers : GeneralItem {
+
+    private Hand other;
+    private HandCollider otherCollider;
+    private bool coverOn;
+    [SerializeField]
+    private GameObject cover;
+    [SerializeField]
+    private Interactable rightOpeningSpot;
+    [SerializeField]
+    private Interactable wrongOpeningSpot;
+
     // Start is called before the first frame update
-    protected override void Start()
-    {
+    protected override void Start() {
         base.Start();
+        objectType = ObjectType.Tweezers;        
+        coverOn = true;
+        Type.On(InteractableType.Interactable);
+    }
+    public override void OnGrab(Hand hand) {
+        base.OnGrab(hand);
+        other = hand.Other;        
+        otherCollider = other.HandCollider;  
+        bool openCover = VRInput.GetControlDown(other.HandType, Controls.TakeMedicine);
+        
+        if (openCover && coverOn && (GameObject.ReferenceEquals(rightOpeningSpot, otherCollider.GetClosestInteractable()) || GameObject.ReferenceEquals(wrongOpeningSpot, otherCollider.GetClosestInteractable()))) {
+            OpenCover();
+        }
+        
+    }
+    public void OpenCover() {
+        coverOn = false;
+        cover.SetActive(false);
     }
 
 }
--- Assets/Scripts/Utilities/Events/EventType.cs
+++ Assets/Scripts/Utilities/Events/EventType.cs
@@ -37,5 +37,6 @@
     TransferLiquidToBottle,
     AttachFilter,
     PlateOpened,
-    AttachPipe
+    AttachPipe,
+    OpenTweezersPackage
 }
