6bcf0640432887ce732c67c1f167698b2300aedd
vrfarmasia <vrfarmasia@gmail.com>
2022-04-25 14:49:03 +0300

    Agar takes time

--- Assets/Scripts/Objects/Agar.cs
+++ Assets/Scripts/Objects/Agar.cs
@@ -3,9 +3,10 @@ using UnityEngine.Events;
 using System.Collections;
 public class Agar : Interactable {
 
-    private bool fingers = false;
-    private bool thumb = false; 
+    private bool FirstTouch = false;
+    private bool SecondTouch = false; 
     private bool isLeft;
+    private System.DateTime LastTouched;
 
     protected override void Start() {
         base.Start();
@@ -13,40 +14,55 @@ public class Agar : Interactable {
     }
 
     public override void Interact(Hand hand) {
+        LastTouched = System.DateTime.Now;
+        
+    }
+
+    public override void Uninteract(Hand hand) {
+        base.Uninteract(hand);
         string handIs = hand.HandType.ToString();
-        if(!fingers) {
+
+        var time = System.DateTime.Now - LastTouched;
+        Logger.Print("fingerprints given for "+ time);
+
+        if (!FirstTouch && time.Seconds > 0) {
             base.Interact(hand);
             if (handIs.Equals("LeftHand")) {
                 isLeft = true;
             } else {
                 isLeft = false;
             }
-            fingers = true;
+            FirstTouch = true;
             Logger.Print("fingerprints given");
-        } else if (!thumb) {
+
+            if (time.Seconds < 4 || time.Seconds > 6) {
+                Task.CreateTaskMistake(TaskType.Fingerprints, "Kosketuksen tulee olla noin 5 sekuntia", 1);
+            }
+
+        } else if (FirstTouch && !SecondTouch) {
             base.Interact(hand);
-            thumb = true;
+            SecondTouch = true;
             Logger.Print("thumb print given");
 
             if (handIs.Equals("LeftHand")) {
                 Events.FireEvent(EventType.FingerprintsGivenL);
-                if (!isLeft){
-                    Task.CreateTaskMistake(TaskType.Fingerprints, "Älä sekoita sormia! Tässä on nyt vasemman käden jäljet.", 2);
+                if (!isLeft) {
+                    Task.CreateTaskMistake(TaskType.Fingerprints, "Älä sekoita sormia! Tässä on nyt vasemman käden jäljet.", 1);
                 } else {
                     Logger.Print("oikein meni");
                 }
             } else {
                 Events.FireEvent(EventType.FingerprintsGivenR);
-                if (isLeft){
-                    Task.CreateTaskMistake(TaskType.Fingerprints, "Älä sekoita sormia! Tässä on nyt oikean käden jäljet.", 2);
+                if (isLeft) {
+                    Task.CreateTaskMistake(TaskType.Fingerprints, "Älä sekoita sormia! Tässä on nyt oikean käden jäljet.", 1);
                 } else {
                     Logger.Print("oikein meni");
                 }
-            } 
-            thumb = true;
+            }
+            SecondTouch = true;
         } else {
             Logger.Print("excess touch");
-            Task.CreateGeneralMistake("Älä koske aineeseen", 1);
         }
+
     }
 }
