ef7a0968954a51bd1da6f3c10a0c0a68d2619b01
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2022-07-03 18:53:46 +0300

    added logic to AsepticClothingPoster

---
+++ Assets/Scripts/Objects/AsepticClothingPoster.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class AsepticClothingPoster : MonoBehaviour {
+
+    private Color highlightedColor;
+
+    public TextMeshPro headCoverText;
+    public TextMeshPro faceMaskText;
+    public TextMeshPro labCoatText;
+    public TextMeshPro sleeveCoversText;
+    public TextMeshPro protectiveGlovesText;
+    public TextMeshPro shoeCoversText;
+
+    public void Start() {
+        highlightedColor = new Color(37, 170, 225);
+    }
+
+    public void HighlightText(string text) {
+        if (text.Equals("Suojapäähine")) headCoverText.color = highlightedColor;
+        else if (text.Equals("Kasvomaski")) faceMaskText.color = highlightedColor;
+        else if (text.Equals("Laboratoriotakki")) labCoatText.color = highlightedColor;
+        else if (text.Equals("Hihasuojat")) sleeveCoversText.color = highlightedColor;
+        else if (text.Equals("Suojakäsineet")) protectiveGlovesText.color = highlightedColor;
+        else if (text.Equals("Kengänsuojat")) shoeCoversText.color = highlightedColor;
+    }
+}
--- Assets/Scripts/Objects/ProtectiveClothing.cs
+++ Assets/Scripts/Objects/ProtectiveClothing.cs
@@ -1,15 +1,20 @@
-﻿public class ProtectiveClothing : GeneralItem {
+﻿using UnityEngine;
 
+public class ProtectiveClothing : GeneralItem {
+
+    private AsepticClothingPoster poster;
     public string type;
 
     protected override void Start() {
         base.Start();
         Type.On(InteractableType.Interactable);
+        poster = GameObject.FindGameObjectWithTag("AsepticClothingPoster").GetComponent<AsepticClothingPoster>();
     }
 
     // Change this to OnTriggerEnter with the players collider
     public override void OnGrabStart(Hand hand) {
         base.OnGrab(hand);
         Events.FireEvent(EventType.ProtectiveClothingEquipped, CallbackData.Object(this));
+        poster.HighlightText(type);
     }
 }
