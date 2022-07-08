7a0e5ac0b83e203e45224968968591c2bc137f1b
tjvalkonen <tomas.valkonen@gmail.com>
2022-07-08 22:57:18 +0300

    Changes to handwashing colliders

--- Assets/Scripts/Objects/Equipment/HandWashingLiquid.cs
+++ Assets/Scripts/Objects/Equipment/HandWashingLiquid.cs
@@ -22,7 +22,9 @@ public class HandWashingLiquid : Interactable {
     }
 
     public override void Interact(Hand hand) {
+
         base.Interact(hand);
+
         Events.FireEvent(EventType.WashingHands, CallbackData.Object(this));
 
         if (!running) {
@@ -32,7 +34,24 @@ public class HandWashingLiquid : Interactable {
 
         }
         running = false;
+
     }
+
+    /*
+     * OnTriggerEnter or on collision?
+    private void OnCollisionEnter(Collision collision) {
+        Events.FireEvent(EventType.WashingHands, CallbackData.Object(this));
+
+        if (!running) {
+            running = true;
+            // Logger.Print("Soap ON!");
+            PlayFX();
+
+        }
+        running = false;
+
+    }
+    */
 
     public void PlayFX() {
         if (particleSystem != null) particleSystem.Play();
