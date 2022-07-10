8fca5d6769db8f3737b7af0c7d898ebefa962b09
tjvalkonen <tomas.valkonen@gmail.com>
2022-07-10 21:38:27 +0300

    AudioFX HandSparkle added.

---
+++ Assets/Scripts/HandSparkleAudioFX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandSparkleAudioFX : MonoBehaviour {
+
+    private AudioSource audioFX;
+
+    public void Start() {
+        audioFX = GetComponent<AudioSource>();
+        Subscribe();
+    }
+
+    public void Subscribe() {
+        Events.SubscribeToEvent(WashingHands, EventType.WashingHands);
+    }
+
+    private void WashingHands(CallbackData data) {
+        var liquid = (data.DataObject as HandWashingLiquid);
+        if (liquid.type.Equals("HandSanitizer"))
+            PlayAudioFX();
+    }
+
+    private void PlayAudioFX() {
+        audioFX.Play();
+    }
+}
