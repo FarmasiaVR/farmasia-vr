9391965cd1e2b2e7a12e50198f3f966088cfdb5f
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2022-10-26 22:32:52 +0300

    Reverted main menu changes

--- Assets/Scripts/MainMenu/MainMenuFunctions.cs
+++ Assets/Scripts/MainMenu/MainMenuFunctions.cs
@@ -9,10 +9,6 @@ public class MainMenuFunctions : MonoBehaviour {
     public GameObject submenuButtons;
     public static bool startFromBeginning = true;
 
-    // using enum for selecting from multiple game proggrespoints.
-    public enum SelectedAutoplay { Beginning, Workspace, CloseSettlePlates, CloseFingertipPlates, FilterHalvesToBottles };
-    static public SelectedAutoplay selectedAutoplay = SelectedAutoplay.Beginning;
-
     private void Start() {
         levelChanger = GameObject.FindGameObjectWithTag("LevelChanger").GetComponent<SceneLoader>();
     }
@@ -33,33 +29,13 @@ public class MainMenuFunctions : MonoBehaviour {
     }
 
     public void SetStartingPoint(string startingPoint) {
-        // if (startingPoint.Equals("PreperationRoom")) startFromBeginning = true;
-        // else if (startingPoint.Equals("WorkspaceRoom")) startFromBeginning = false;
-        // else if (startingPoint.Equals("WorkspaceRoom")) startFromBeginning = false;
-        if (startingPoint.Equals("PreperationRoom")) {
-            startFromBeginning = true;
-            selectedAutoplay = SelectedAutoplay.Beginning;
-        } else if (startingPoint.Equals("WorkspaceRoom")) {
-            startFromBeginning = false;
-            selectedAutoplay = SelectedAutoplay.Workspace;
-        } else if (startingPoint.Equals("CloseSettlePlates")) {
-            startFromBeginning = false;
-            selectedAutoplay = SelectedAutoplay.CloseSettlePlates;
-        } else if (startingPoint.Equals("CloseFingertipPlates")) {
-            startFromBeginning = false;
-            selectedAutoplay = SelectedAutoplay.CloseFingertipPlates;
-        } else if (startingPoint.Equals("FilterHalvesToBottles")) {
-            startFromBeginning = false;
-            selectedAutoplay = SelectedAutoplay.FilterHalvesToBottles;
-        }
-
+        if (startingPoint.Equals("PreperationRoom")) startFromBeginning = true;
+        else if (startingPoint.Equals("WorkspaceRoom")) startFromBeginning = false;
         LoadScene(sceneType);
     }
 
     public void ExitGame() {
         Application.Quit();
-        
-        // Disabled for the build
         // UnityEditor.EditorApplication.isPlaying = false;
     }
 }
