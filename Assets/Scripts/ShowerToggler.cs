f1b56f675e4cdc9983e2dcf20f2e0d06080711e4
juhkure <johannes.pyykonen@helsinki.fi>
2023-02-22 15:03:18 +0200

    Move ShowerToggler to script folder

--- Assets/Scripts/ShowerToggler.cs
+++ Assets/Scripts/ShowerToggler.cs
@@ -6,19 +6,27 @@ using UnityEngine;
 public class ShowerToggler : MonoBehaviour
 {
     public bool open;
+    public AudioSource audioSource;
+    public float loopStartTime;
+    public float loopEndTime;
     public ParticleSystem water;
     private Coroutine turnOffShowerCoroutine;
+    public ShowerExtinguisher showerExtinguisher;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (open)
+        {
+            showerExtinguisher.Extinguish();
+        }
     }
 
     // Initiates water VFX, terminates previous turnOffShower coroutine
@@ -28,6 +36,14 @@ public class ShowerToggler : MonoBehaviour
         {
             open = true;
             water.Play();
+            
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+                audioSource.time = 0;
+            }
+
+
             Debug.Log("Shower should be on");
             
             // Stop the previous coroutine if it's still running
@@ -39,10 +55,25 @@ public class ShowerToggler : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Shower")
+        {
+            if (audioSource.time >= loopEndTime)
+            {
+                audioSource.time = loopStartTime;
+            }
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Shower" && open)
         {
+
+            audioSource.time = loopEndTime;
+            audioSource.loop = false;
+
             turnOffShowerCoroutine =  StartCoroutine(turnOffShower());
         }
     }
@@ -50,20 +81,15 @@ public class ShowerToggler : MonoBehaviour
     // Coroutine that turns off water vfx after 4 seconds have passed
     private IEnumerator turnOffShower()
     {
-        open = false;
         Debug.Log("Shower should still be on for next 4 seconds");
 
         yield return new WaitForSeconds(4);
 
-        if (!open)
-        {
-            water.Stop();
-            Debug.Log("Shower should now be off");
-        }
-        else
-        {
-            Debug.Log("Cancelled turnoff coroutine");
-        }
+        open = false;
+
+        water.Stop();
+        Debug.Log("Shower should now be off");
+        
     }
 
 
