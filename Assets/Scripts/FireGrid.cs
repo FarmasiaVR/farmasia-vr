fcbe17cb83d0e7bc620ca810e01ebef024b41bc1
NuiS4ncE <nuis4nce@hotmail.com>
2023-02-24 04:11:02 +0200

    added smokevfx to the firegridobject prefab and uncommented play and stop lines

--- Assets/Scripts/FireGrid.cs
+++ Assets/Scripts/FireGrid.cs
@@ -58,7 +58,7 @@ public class FireGrid : MonoBehaviour
     public void Extinguish()
     {
         fireVFX.Stop();
-        //smokeVFX.SetFloat("Spawn Rate", 0f);
+        smokeVFX.SetFloat("Spawn Rate", 0f);
         pointLight.SetActive(false);
         if (extinguishParticle != null && isIgnited == true)
         {
@@ -74,7 +74,7 @@ public class FireGrid : MonoBehaviour
     public void Ignite()
     {
         fireVFX.Play();
-        //smokeVFX.SetFloat("Spawn Rate", 50f);
+        smokeVFX.SetFloat("Spawn Rate", 50f);
         pointLight.SetActive(true);
         if (igniteParticle != null && isIgnited == false)
         {
