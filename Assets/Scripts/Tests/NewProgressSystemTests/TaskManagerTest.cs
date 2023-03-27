6f235a3036b2188c5faeddc69414dfb277546536
opturtio <olli-pekka.turtio@helsinki.fi>
2023-03-27 19:29:05 +0300

    Created setUp for TaskManagerTest script

--- Assets/Scripts/Tests/NewProgressSystemTests/TaskManagerTest.cs
+++ Assets/Scripts/Tests/NewProgressSystemTests/TaskManagerTest.cs
@@ -4,27 +4,32 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
-public class SampleTest
+public class TaskManagerTest
 {
+    [SetUp]
+    public void SetUp()
+    {
+        LogAssert.ignoreFailingMessages = true;
+        SceneManager.LoadScene("TaskManagerExample", LoadSceneMode.Single);
+    }
 
     [UnityTest]
     public IEnumerator TaskManagerExistsInTestScene()
     {
-        LogAssert.ignoreFailingMessages = true;
         yield return new EnterPlayMode();
-        SceneManager.LoadScene("TaskManagerExample", LoadSceneMode.Single);
         yield return null; // <--- skip frame
+        
         //Warning: this supposes that there is exaclty one GameManager object in that scene that we loaded
         GameObject testObject = GameObject.Find("GameManager");
         TaskManager manager = testObject.GetComponent<TaskManager>();
         Assert.IsNotNull(manager);
 
-        
-
-        Debug.Log("hello world from test scene!");
-
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
         yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator CreatingTaskListWorks()
+    {
+
     }
 }
