ef83b8052d0946b81f6e31ef368d733483b15d46
opturtio <olli-pekka.turtio@helsinki.fi>
2023-04-22 12:31:59 +0300

    Created FlameController script to control the scale of the "Flames" GameObject. The script creates better looking flame effect by increasing it from 0 to a maximum of 2, resulting in a more realistic effect.

--- Assets/Scripts/FlameController.cs
+++ Assets/Scripts/FlameController.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class FlameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private VisualEffect fireVFX;
+
+    [SerializeField]
+    private float x = 0.0f;
+    [SerializeField]
+    private float y = 0.0f;
+    [SerializeField]
+    private float z = 0.0f;
+
+    // Summary
+    // This method is called once when the GameObject is first enabled.
+    // It fetches the VisualEffect component the "Flames" and sets its
+    // initial scale using the SetScale method.
     void Start()
     {
-        
+        fireVFX = transform.Find("Flames").gameObject.GetComponent<VisualEffect>();
+        SetScale(x, y, z);
     }
 
-    // Update is called once per frame
+    // Summary
+    // Update method is called once per frame to update the scale of the GameObject "Flames".
+    // The maximum size is set to 2. It will increase the value of all the axis by 0.03 when
+    // scale is smaller that 0.5 and by 0.02 when the scale is smaller that 2.
+    // Finally, the updated values are passed to the SetScale method.
     void Update()
     {
-        
+        if (x >= 2f && y >= 2f && z >= 2f)
+        {
+            x = 2f;
+            y = 2f;
+            z = 2f;
+        }
+        if (x < 0.5f && y < 0.5f && z < 0.5f)
+        {
+            x += 0.03f * Time.deltaTime;
+
+            y += 0.03f * Time.deltaTime;
+
+            z += 0.03f * Time.deltaTime;
+        }
+
+        if (x < 2f && y < 2f && z < 2f)
+        {
+            x += 0.02f * Time.deltaTime;
+            y += 0.02f * Time.deltaTime;
+            z += 0.02f * Time.deltaTime;
+        }
+        SetScale(x, y, z);
+    }
+
+    // Summary
+    // This method sets the local scale of the "Flames" GameObject using float values x, y, z.
+    private void SetScale(float x, float y, float z)
+    {
+        fireVFX.transform.localScale = new Vector3(x, y, z);
     }
 }
