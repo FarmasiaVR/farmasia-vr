b18ec26ed3d07cb27f3f302e3d3759709a2837db
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-04-27 21:31:58 +0300

    remove dependency

--- Assets/XRLuerlock.cs
+++ Assets/XRLuerlock.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Policy;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 using UnityEngine.XR.Interaction.Toolkit;
 
 
