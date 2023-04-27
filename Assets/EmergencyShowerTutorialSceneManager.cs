4744d94242f4e4628e1c58babd63102dfe80affb
Nikola <55931311+NuiS4ncE@users.noreply.github.com>
2023-04-27 15:35:57 +0300

    preliminary work for emergencyshowertutorial

---
+++ Assets/EmergencyShowerTutorialSceneManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmergencyShowerTutorialSceneManager : MonoBehaviour
+{
+    private TaskManager taskManager;
+    private TaskboardManager taskboardManager;
+    private bool playerExtinguished, doneOnce;
+    
+    private void Awake()
+    {
+        taskManager = GetComponent<TaskManager>();
+        taskboardManager = GetComponent<TaskboardManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CheckPlayerExtinguished() && !doneOnce)
+        {
+            taskManager.CompleteTask("PlayerOnFire");
+        }
+    }
+
+    private bool CheckPlayerExtinguished()
+    {
+        return playerExtinguished;
+    }
+
+    public void SetPlayerExtinguished()
+    {
+        playerExtinguished = true;
+        doneOnce = true;
+    }
+
+}
+
