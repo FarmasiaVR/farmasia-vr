d6a6c396051302f8bdae9468710490a35c6a6dc9
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-07-12 11:06:32 +0300

    first version of playerSelectedFilter preventing hover

--- Assets/playerSelectedFilter.cs
+++ Assets/playerSelectedFilter.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Filtering;
 
-public class playerSelectedFilter : MonoBehaviour, IXRSelectFilter
+public class playerSelectedFilter : MonoBehaviour, IXRSelectFilter, IXRHoverFilter
 {
+
+    public bool preventSelect;
+    public bool preventHover;
     public bool canProcess => true;
 
+    //This Process is for when interactor is selecting an interactable
     public bool Process(IXRSelectInteractor interactor, IXRSelectInteractable interactable)
     {
-        List<IXRSelectInteractor> selectors = interactable.interactorsSelecting;
+        if (preventSelect)
+        {
+            List<IXRSelectInteractor> selectors = interactable.interactorsSelecting;
 
-        bool playerIsSelecting = playerIsOneOfSelectors(selectors);
+            bool playerIsSelecting = playerIsOneOfSelectors(selectors);
 
-        return playerIsSelecting;
+            return playerIsSelecting;
+        }
+        else
+        {
+            return true;
+        }
     }
 
+    //this Process is for when interactor is hovering an interactable
+    public bool Process(IXRHoverInteractor interactor, IXRHoverInteractable interactable)
+    {
+        if (preventHover)
+        {
+            IXRSelectInteractable selectableInteractable = interactable.transform.GetComponent<IXRSelectInteractable>();
+            
+            List<IXRSelectInteractor> selectors = selectableInteractable.interactorsSelecting;
+
+            bool playerIsSelecting = playerIsOneOfSelectors(selectors);
+
+            return playerIsSelecting;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
     bool playerIsOneOfSelectors(List<IXRSelectInteractor> selectors)
     {
         bool playerIsSelecting = false;
