be0acb80d5418fcdc20fc78763ce2ea89a8c4de9
SamiP7 <sami.parkkinenn@gmail.com>
2023-07-18 04:23:41 +0300

    writing updates error message immediately

--- Assets/Scripts/UISystem/WritingOptions/WritingOptions.cs
+++ Assets/Scripts/UISystem/WritingOptions/WritingOptions.cs
@@ -107,6 +107,8 @@ public class WritingOptions : MonoBehaviour {
             resultText += line + "\n";
         }
         resultTextField.SetText(resultText);
+
+        UpdateErrorMessage();
     }
 
     private void UpdateErrorMessage() {
