f3633be1dd12042a4a1a1f7db2d5ab2835338fe5
opturtio <olli-pekka.turtio@helsinki.fi>
2023-08-11 00:45:29 +0300

    Created smoke extinguish effect for firegrid.

--- Assets/Scripts/FlameExtinguish.cs
+++ Assets/Scripts/FlameExtinguish.cs
@@ -9,18 +9,29 @@ public class FlameExtinguish : MonoBehaviour
     private VisualEffect fireVFX;
 
     [SerializeField]
+    private VisualEffect extinguishParticles;
+
+    [SerializeField]
     private float x;
     [SerializeField]
     private float y;
     [SerializeField]
     private float z;
+    [SerializeField]
+    private float multiplier;
 
+    private bool playedOnce = false;
     // Summary
     // This method is called once when the GameObject is first enabled.
     // It fetches the VisualEffect component the "Flames" and it's scale.
     void Start()
     {
         fireVFX = transform.Find("Flames").gameObject.GetComponent<VisualEffect>();
+
+        extinguishParticles = transform.Find("ExtinguishParticlesVFX").gameObject.GetComponent<VisualEffect>();
+
+        extinguishParticles.Stop();
+
         Vector3 scale = fireVFX.transform.localScale;
         x = scale.x;
         y = scale.y;
@@ -31,16 +42,27 @@ public class FlameExtinguish : MonoBehaviour
     // 
     void Update()
     {
-        x -= 0.006f;
-        y -= 0.006f;
-        z -= 0.006f;
+        x -= 0.006f * multiplier;
+        y -= 0.006f * multiplier;
+        z -= 0.006f * multiplier;
         if (x <= 0f && y <= 0f && z <= 0f)
         {
             x = 0f;
             y = 0f;
             z = 0f;
         }
+        if (!playedOnce)
+        {
+            if (x >= 0.1f && x <= 0.2f && y >= 0.1f && y <= 0.2f && z >= 0.1f && z <= 0.2f)
+            {
+                extinguishParticles.Play();
+                playedOnce = true;
+            }
+        }
+
+
         SetScale(x, y, z);
+
     }
 
     // Summary
