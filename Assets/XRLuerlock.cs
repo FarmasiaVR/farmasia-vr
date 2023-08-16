15b346a1c892e6e143af97fcde148074919bc249
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-08-16 10:36:07 +0300

    luerlock now detaches syringe if the selecting hand goes too far from the luerlock

--- Assets/XRLuerlock.cs
+++ Assets/XRLuerlock.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
-
+using System.Collections.Generic;
+using UnityEditorInternal;
 
 //this is crazy prototyping code from every software devs nightmares, do not try this at home
 public class XRLuerlock : MonoBehaviour
@@ -12,7 +13,7 @@ public class XRLuerlock : MonoBehaviour
 
     public XRJoinSyringeToLuerlock syringe1Socket;
     public XRJoinSyringeToLuerlock syringe2Socket;
-
+    public float detachDistance;
     enum transferMode
     {
         take,
@@ -25,9 +26,43 @@ public class XRLuerlock : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        luerlockSyringeDetachUpdate();
+    }
 
+    //Checks all interactors who are currently selecting(grabbing) the luerlock
+    //and detaches a syringe if selector is detachDistance away from a socket
+    void luerlockSyringeDetachUpdate()
+    {
+        List<IXRSelectInteractor> interactors = interactableToTrack.interactorsSelecting;
+        foreach (var interactor in interactors)
+        {
+            float distanceToClosestSocket = getDistanceToClosestSocket(interactor.transform.position);
+            if (distanceToClosestSocket > detachDistance)
+            {
+                detachSyringeFromClosestSocket(interactor.transform.position);
+            }
+        }
+    }
+
+    float getDistanceToClosestSocket(Vector3 pos)
+    {
+        Vector3 socket1Pos = syringe1Socket.transform.position;
+        Vector3 socket2Pos = syringe2Socket.transform.position;
+
+
+        float distanceToSocket1 = Vector3.Distance(socket1Pos, pos);
+        float distanceToSocket2 = Vector3.Distance(socket2Pos, pos);
+
+        if (distanceToSocket1 < distanceToSocket2)
+        {
+            return distanceToSocket1;
+        }
+        else
+        {
+            return distanceToSocket2;
+        }
     }
 
     XRJoinSyringeToLuerlock findClosestSocket(Vector3 pos)
@@ -38,9 +73,6 @@ public class XRLuerlock : MonoBehaviour
 
         float distanceToSocket1 = Vector3.Distance(socket1Pos, pos);
         float distanceToSocket2 = Vector3.Distance(socket2Pos, pos);
-        Debug.Log("Finding closest socket");
-        Debug.Log(distanceToSocket1 + "   " + syringe1Socket.gameObject.name);
-        Debug.Log(distanceToSocket2 + "   " + syringe2Socket.gameObject.name);
 
         if (distanceToSocket1 < distanceToSocket2)
         {
@@ -81,8 +113,13 @@ public class XRLuerlock : MonoBehaviour
     {
         IXRActivateInteractor interactor = args.interactorObject;
 
-        XRJoinSyringeToLuerlock closestSocket = findClosestSocket(interactor.transform.position);
-        XRJoinSyringeToLuerlock furtestSocket = findFurthestSocket(interactor.transform.position);
+        detachSyringeFromClosestSocket(interactor.transform.position);
+    }
+
+    void detachSyringeFromClosestSocket(Vector3 interactorPosition)
+    {
+        XRJoinSyringeToLuerlock closestSocket = findClosestSocket(interactorPosition);
+        XRJoinSyringeToLuerlock furtestSocket = findFurthestSocket(interactorPosition);
 
         //if the other socket has no object attached, disable it to prevent it from stealing the detached syringe again.
         if (!furtestSocket.objAttached)
@@ -91,8 +128,6 @@ public class XRLuerlock : MonoBehaviour
         }
 
         closestSocket.detachSyringe();
-       
-       
     }
 
     public void sendMedicine(InputAction.CallbackContext context)
