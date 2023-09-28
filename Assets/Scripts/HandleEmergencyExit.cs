8dd7ba95380a9d4033acde12984bb6639f9db736
Roni Hokkanen <roni.hokkanen@protonmail.com>
2023-09-28 19:15:24 +0300

    Fix copypaste mistake

--- Assets/Scripts/HandleEmergencyExit.cs
+++ Assets/Scripts/HandleEmergencyExit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandleExit : MonoBehaviour
+public class HandleEmergencyExit : MonoBehaviour
 {
     private SceneLoader levelChanger;
     // Start is called before the first frame update
