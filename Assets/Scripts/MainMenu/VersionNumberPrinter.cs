62b58ef9a6ef0a9b129932ad6ebf12a84b3f927d
opturtio <olli-pekka.turtio@helsinki.fi>
2023-10-17 17:16:44 +0300

    Created translations for manuals and rest of the texts in MainMenu scene

--- Assets/Scripts/MainMenu/VersionNumberPrinter.cs
+++ Assets/Scripts/MainMenu/VersionNumberPrinter.cs
@@ -7,10 +7,12 @@ public class VersionNumberPrinter : MonoBehaviour
 {
     public string versionPrefix;
     private TextMeshPro versionText;
+    private string applicationVersion;  // The local variable for the application version
 
     void Start()
     {
         versionText = GetComponent<TextMeshPro>();
-        versionText.text = versionPrefix + Application.version;
+        applicationVersion = Application.version;  // Assign the application version to the local variable
+        versionText.text += applicationVersion;    // Use the local variable here
     }
 }
