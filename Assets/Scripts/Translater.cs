f4442ee45308954b0fbf92e7daa3f7f5374e34c8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-10-25 12:45:24 +0300

    use async/await in Translater
    Co-Authored-By: opturtio <86497253+opturtio@users.noreply.github.com>

--- Assets/Scripts/Translater.cs
+++ Assets/Scripts/Translater.cs
@@ -1,14 +1,14 @@
+using System.Threading.Tasks;
 using UnityEngine.Localization.Settings;
 
 public static class Translater
 {
-    public static void Translate(string table, string key, System.Action<string> callback)
+    async public static Task<string> Translate(string table, string key)
     {
         var handle = LocalizationSettings.StringDatabase.GetLocalizedStringAsync(table, key);
 
-        handle.Completed += (operationHandle) =>
-        {
-            callback(operationHandle.Result);
-        };
+        await handle.Task;
+
+        return handle.Result;
     }
 }
