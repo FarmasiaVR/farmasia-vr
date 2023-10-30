a5b47d7a8fcc2d388835e2b7abe5121093d5d9a6
Roni Hokkanen <roni.hokkanen@protonmail.com>
2023-10-30 14:25:51 +0200

    Move scene name to scene type mapping to where the scene types are defined

--- Assets/Scripts/MainMenu/SceneLoader.cs
+++ Assets/Scripts/MainMenu/SceneLoader.cs
@@ -8,25 +8,8 @@ public class SceneLoader : MonoBehaviour {
 
     public CameraFadeController fadeController;
 
-    Dictionary<SceneTypes, string> sceneTypeToSceneName = new Dictionary<SceneTypes, string> {
-        { SceneTypes.Restart, "Restart" },
-        { SceneTypes.MainMenu, "MainMenu" },
-        { SceneTypes.MedicinePreparation, "MedicinePreparation 2.0 XR" },
-        { SceneTypes.Tutorial, "ControlsTutorial" },
-        { SceneTypes.MembraneFilteration, "XR MembraneFilteration 2.0" },
-        { SceneTypes.ChangingRoom, "ChangingRoom" },
-        { SceneTypes.FireHazard, "Laboratory" },
-        { SceneTypes.FireExtinguisherTutorial, "FireExtinguisherTutorial" },
-        { SceneTypes.FireBlanketTutorial, "FireBlanketTutorial" },
-        { SceneTypes.EyeShowerTutorial, "EyeShowerTutorial" },
-        { SceneTypes.EmergencyShowerTutorial, "EmergencyShowerTutorial" },
-        { SceneTypes.EmergencyExitTutorial, "EmergencyExitTutorial" },
-        { SceneTypes.EmergencyExit, "EmergencyExit" },
-        { SceneTypes.EmergencyExit1, "EmergencyExit 1" }
-    };
-
     public void SwapScene(SceneTypes type) {
-        ChangeScene(sceneTypeToSceneName[type]);
+        ChangeScene(GameScenes.GetName(type));
     }
 
     public void SwapScene(int type) => SwapScene((SceneTypes) type);
