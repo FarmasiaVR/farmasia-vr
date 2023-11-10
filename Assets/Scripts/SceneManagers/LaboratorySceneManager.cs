f4686683c6ca8d19128441b59e726480eae48db2
Roni Hokkanen <roni.hokkanen@protonmail.com>
2023-11-10 22:54:54 +0200

    Simplify laboratory scene manager a lot and move tutorial button activations to equipment found zones

--- Assets/Scripts/SceneManagers/LaboratorySceneManager.cs
+++ Assets/Scripts/SceneManagers/LaboratorySceneManager.cs
@@ -10,72 +10,16 @@ public class LaboratorySceneManager : MonoBehaviour
     private PlayerEnter[] playerEnter;
     private bool fireBlanketFound, eyeShowerFound, extinguisherFound, emergencyShowerFound;
 
-    private void Awake()
-    {
+    private void Awake() {
         taskManager = GetComponent<TaskManager>();
         taskboardManager = GetComponent<TaskboardManager>();
         playerEnter = FindObjectsOfType<PlayerEnter>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (iterateThrough())
-        {
-            IterateFindPlayerEnterScripts();
-        }
-    }
-
-    /// <summary>
-    /// Iterates through PLayerEnter script components present in the Laboratory scene. 
-    /// </summary>
-    public void IterateFindPlayerEnterScripts()
-    {
-        foreach (PlayerEnter script in playerEnter)
-        {
-            if (script.gameObject.name.Contains("PlayerEnterBox FireBlanket") && script.HasEnteredOnce() && !fireBlanketFound)
-            {
-                taskboardManager.MarkTaskAsCompleted("FireBlanket");
-                taskManager.CompleteTask("FireBlanket");
-                fireBlanketFound = true;
-            }
-            if (script.gameObject.name.Contains("PlayerEnterBox EyeShower") && script.HasEnteredOnce() && !eyeShowerFound)
-            {
-                taskboardManager.MarkTaskAsCompleted("EyeShower");
-                taskManager.CompleteTask("EyeShower");
-                eyeShowerFound = true;
-            }
-            if (script.gameObject.name.Contains("PlayerEnterBox FireExtinguisher") && script.HasEnteredOnce() && !extinguisherFound)
-            {
-                taskboardManager.MarkTaskAsCompleted("Extinguisher");
-                taskManager.CompleteTask("Extinguisher");
-                extinguisherFound = true;
-            }
-            if (script.gameObject.name.Contains("PlayerEnterBox EmergencyShower") && script.HasEnteredOnce() && !emergencyShowerFound)
-            {
-                taskboardManager.MarkTaskAsCompleted("EmergencyShower");
-                taskManager.CompleteTask("EmergencyShower");
-                emergencyShowerFound = true;
-            }
-        }
-    }
-
-    /// <summary>
-    /// Checks, if equipment has been found in the scene.
-    /// </summary>
-    /// <returns>Returns false if fireBlanketFound, eyeShowerFound, extinguisherFound and emergencyShowerFound are true. Else true.</returns>
-    private bool iterateThrough()
-    {
-        if (fireBlanketFound && eyeShowerFound && extinguisherFound && emergencyShowerFound)
-        {
-            return false;
-        }
-        return true;
+    public void CompleteTask(string taskName) {
+        taskManager.CompleteTask(taskName);
+        taskboardManager.MarkTaskAsCompleted(taskName);
+        //TODO: Move TaskList into TaskManager and maybe add the option to directly read the 
+        // tasklist hash table (shouldn't be able to write)
     }
 }
