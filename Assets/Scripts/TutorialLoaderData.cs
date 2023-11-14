87670a0e19ff67af07f99f17b141d03b0ca435c7
Roni Hokkanen <roni.hokkanen@protonmail.com>
2023-11-14 17:30:40 +0200

    Add new script for loading into a specific tutorial of the equipment tutorials scene

---
+++ Assets/Scripts/TutorialLoaderData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public enum TutorialType {
+    FireExtinguisher, FireBlanket, EmergencyShower, EyeShower
+}
+
+public class TutorialLoaderData {
+    public TutorialType? tutorialType; 
+}
