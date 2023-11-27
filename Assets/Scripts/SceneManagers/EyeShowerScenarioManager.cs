aaf53b4a656941033f449b5485d4e3acfc4a699f
Saukka <sauli.sorvari@kolumbus.fi>
2023-11-27 04:24:43 +0200

    New eyeshower scenario and scene manager for it

---
+++ Assets/Scripts/SceneManagers/EyeShowerScenarioManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeShowerScenarioManager : MonoBehaviour
+{
+
+    public GameObject levelClearedObject;
+    private bool liquidBursted = false;
+    public ParticleSystem particleSystemToActivate;
+    public GameObject burstPopUp;
+
+
+    public void Start()
+    {
+        if (particleSystemToActivate != null)
+        {
+            particleSystemToActivate.Stop();
+        }
+    }
+    public void Aim() {
+
+        if (liquidBursted)
+        {
+            levelClearedObject.SetActive(true);
+        }
+        
+    }
+
+    public void BurstLiquid()
+    {
+        if (liquidBursted)
+        {
+            return;
+        }
+        liquidBursted = true;
+        if (particleSystemToActivate != null)
+        {
+            particleSystemToActivate.Play();
+        }
+
+        StartCoroutine(PopUpCoroutine());
+    }
+
+    private IEnumerator PopUpCoroutine()
+    {
+        burstPopUp.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+
+        burstPopUp.SetActive(false);
+    }
+}
+
