d10b291a399964ef3bf2cfc4c4b0b7fce2edd19a
Juho Siitonen <juho_tuomas@msn.com>
2024-10-15 21:07:49 +0300

    Popup and sound effect added for the skip button in MF scene

--- Assets/Scripts/RoomFunctions/MembraneFilteration/SkipToAsepticSpaceButton.cs
+++ Assets/Scripts/RoomFunctions/MembraneFilteration/SkipToAsepticSpaceButton.cs
@@ -7,12 +7,13 @@ public class SkipToAsepticSpaceButton : MonoBehaviour
 
     public GameObject helperObject; // Reference to the GameObject with the ManualTester'sLilHelper component
 
-    // This method will be called from the button
     public void ActivateManualTesterHelper()
     {
         if (helperObject != null)
         {
-            helperObject.SetActive(true); 
+            helperObject.SetActive(true);
+            UISystem.Instance.CreatePopup(Translator.Translate("XR MembraneFilteration 2.0", "SkipToAsepticSpaceButton"), MsgType.Notify);
+            G.Instance.Audio.Play(AudioClipType.TaskCompletedBeep);
         }
         else
         {
