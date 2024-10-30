9183befd6677a1af8516963f49a61ab2729f97ec
levomaaa <148433955+levomaaa@users.noreply.github.com>
2024-10-30 02:38:21 +0200

    LaboratoryTour whiteboard translations

--- Assets/Scripts/Managers/TaskDescriptionManager.cs
+++ Assets/Scripts/Managers/TaskDescriptionManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -13,45 +12,50 @@ public class TaskDescriptionManager : MonoBehaviour
 
     private void Awake()
     {
-        if (taskDescriptions.Count == 0)
-        {
-            taskDescriptions = new List<TextMeshPro>();
-            foreach (GameObject descObject in GameObject.FindGameObjectsWithTag("TaskDescription"))
-            {
-                taskDescriptions.Add(descObject.GetComponent<TextMeshPro>());
-            }
-        }
+        taskDescriptions = new List<TextMeshPro>();  // Create the list
+    }
+
+    private void Start()
+    {
+        UpdateTaskDescriptionList(); 
     }
 
     private void OnEnable()
     {
         LocalSelector.OnLocaleChanged += UpdateGameOverText;
-        Debug.Log("Subscribed to OnLocaleChanged event");
     }
 
     private void OnDisable()
     {
         LocalSelector.OnLocaleChanged -= UpdateGameOverText;
-        Debug.Log("Unsubscribed from OnLocaleChanged event");
     }
 
-    /// <summary>
-    /// Updates the task descriptions in the scene with new info
-    /// </summary>
-    /// <param name="task">The task that will have its info shows in the scene</param>
-    public void UpdateTaskDescriptions(Task task)
+     public void UpdateTaskDescriptionList()
+    {
+        taskDescriptions.Clear();  // Empty the list
+        
+        // Find all objects which have the tag "TaskDescription"
+        foreach (GameObject descObject in GameObject.FindGameObjectsWithTag("TaskDescription"))
+        {
+            TextMeshPro textComponent = descObject.GetComponent<TextMeshPro>();
+            if (textComponent != null)
+            {
+                taskDescriptions.Add(textComponent);
+                textComponent.text = "This should not be shown";
+            }
+        }
+        
+    }
+
+      public void UpdateTaskDescriptions(Task task)
     {
         foreach (TextMeshPro taskDesc in taskDescriptions)
         {
-            taskDesc.text = task.taskText;
+            taskDesc.text = Translator.Translate("LaboratoryTour", task.name); // Update whiteboard text
         }
     }
-    /// <summary>
-    /// Updates all the descriptions to show how many points the player has collected.
-    /// Used for testing and shouldn't be used in the final game
-    /// </summary>
-    /// <param name="taskList">The task list the player has finished</param>
-    public void GameOverText(TaskList taskList)
+
+        public void GameOverText(TaskList taskList)
     {
         currentTaskList = taskList;
         UpdateTaskDescriptionsWithPoints();
@@ -69,7 +73,8 @@ public class TaskDescriptionManager : MonoBehaviour
     {
         foreach (TextMeshPro taskDesc in taskDescriptions)
         {
-            taskDesc.text = Translator.Translate("LaboratoryTour", "MissionCompleted") + " " + currentTaskList.GetPoints();
+            string translatedText = Translator.Translate("LaboratoryTour", "MissionCompleted") + " " + currentTaskList.GetPoints();
+            taskDesc.text = translatedText;
         }
     }
 }
