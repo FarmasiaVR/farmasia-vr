43ecf21d6a6792888a98ec4c558edcccd7e86dc8
UncSald <iikka.alingue@gmail.com>
2025-03-12 23:18:39 +0200

    Added unity events

--- Assets/Scripts/Objects/Equipment/AgarPlateBottom.cs
+++ Assets/Scripts/Objects/Equipment/AgarPlateBottom.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Events;
 
 public class AgarPlateBottom : GeneralItem {
 
