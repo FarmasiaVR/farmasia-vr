97aee9ff61beeccdee0e54edc5af947ce611be9b
Colabres <pavel.allen@gmail.com>
2025-03-15 18:05:51 +0200

    liquid container bug fix. The contamination liquid type is now changed to senders liquid type. event for mistake is invoked if contamination type of finnpipette dosent match the intake liquide type.

--- Assets/Scripts/Objects/Equipment/Pipette.cs
+++ Assets/Scripts/Objects/Equipment/Pipette.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class Pipette : GeneralItem {
 
@@ -12,7 +14,8 @@ public class Pipette : GeneralItem {
     public float defaultPosition, maxPosition;
     
     public Transform handle;
-
+    [Tooltip("This is called when pipette is contaminated")]
+    public UnityEvent<string, int> contaminatedPipeteUsed;
     // The LiquidContainer this Pipette is interacting with
     public LiquidContainer BottleContainer { get; set; }
 
@@ -78,14 +81,18 @@ public class Pipette : GeneralItem {
     }
 
     private void TransferToBottle(bool into) {
-        Logger.Print("This code is runing Doro");
+        
         Logger.Print(into);
         
         if (BottleContainer == null) {
             Logger.Print("Didnt find bottle");    
             return;}
         if (Vector3.Angle(-BottleContainer.transform.up, transform.up) > 25) return;
-
+        Logger.Print("Pippete is trying to get liquid");
+        if(Container.contaminationLiquidType != BottleContainer.LiquidType){
+            Logger.Print("Doro pippete is contaminated with different liquid type");
+            return;
+        }
         Container.TransferTo(BottleContainer, into ? Container.Capacity : -Container.Capacity);
     }
     
@@ -111,9 +118,16 @@ public class Pipette : GeneralItem {
 
     private void TransferToBottleXR(bool into)
     {
+        //Logger.Print("This code is runing Doro v2");
         if (BottleContainer == null) return;
         if (Vector3.Angle(-BottleContainer.transform.up, transform.up) > 25) return;
         if (!into && Vector3.Distance(BottleContainer.transform.position, transform.position) > 0.3f) return;
+        
+        if(Container.contaminationLiquidType != BottleContainer.LiquidType && Container.contaminationLiquidType != LiquidType.None && BottleContainer.LiquidType != LiquidType.None){
+            Logger.Print("Pippete is contaminated with different liquid type");
+            contaminatedPipeteUsed.Invoke("Please don't use this pipette; it is already contaminated",10);
+            return;
+        }
         Container.TransferTo(BottleContainer, into ? LiquidTransferStep : -LiquidTransferStep);
     }
 }
