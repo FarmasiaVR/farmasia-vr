2782a3d05f04e85bd5ded86b613a6468bcebc08b
Colabres <pavel.allen@gmail.com>
2025-03-25 12:44:17 +0200

    TimerButton script created

---
+++ Assets/Scripts/TimerButtonPCM.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerButtonPCM : MonoBehaviour
+{
+
+    public ClockController timerManager; 
+    public bool isStartButton = true; 
+    private void Start()
+    {
+        XRSimpleInteractable interactable = GetComponent<XRSimpleInteractable>();
+        interactable.selectEntered.AddListener(OnButtonPressed);
+    }
+
+    private void OnButtonPressed(SelectEnterEventArgs args)
+    {
+        if (timerManager != null)
+        {
+            if (isStartButton)
+                timerManager.StartTimer();
+            else
+                timerManager.StopTimer();
+        }
+    }
+
+}
