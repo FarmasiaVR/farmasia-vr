1ff87dd791e6178cf716d862ea8c2e9e21fa9f86
Matias Ilola <ilola.matte@gmail.com>
2025-04-10 17:40:19 +0300

    A script for infinite bluesticks

---
+++ Assets/Scripts/PlateCountMethod/BlueStickSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueStickSpawner : MonoBehaviour
+{
+    public string itemTag = "Bluestick";
+    public GameObject BlueStickPrefab;
+    public float checkInterval = 2f;
+    public Transform ItemSpawnPoint;
+    
+    private Collider areaCollider;
+
+    void Start()
+    {
+        areaCollider = GetComponent<Collider>();
+        InvokeRepeating(nameof(CheckAndSpawnItem), 1f, checkInterval);
+    }
+
+    void CheckAndSpawnItem()
+    {
+        Collider[] colliders = Physics.OverlapBox(
+            areaCollider.bounds.center,
+            areaCollider.bounds.extents,
+            Quaternion.identity);
+
+        bool itemFound = false;
+        foreach (var col in colliders)
+        {
+            if (col.CompareTag(itemTag))
+            {
+                itemFound = true;
+                break;
+            }
+        }
+
+        if (!itemFound)
+        {
+            Instantiate(BlueStickPrefab, ItemSpawnPoint.position, ItemSpawnPoint.rotation);
+        }
+    }
+
+}
