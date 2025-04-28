d7ba88698ab0d356814f4e3eeab686641ede9ab7
UncSald <iikka.alingue@gmail.com>
2025-04-28 11:59:41 +0300

    Added toggle for showing the scene results, or the submission panel.

--- Assets/YesNoButtonHandler.cs
+++ Assets/YesNoButtonHandler.cs
@@ -3,15 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class YesNoController : MonoBehaviour
 {
+    public GameObject userInput;
+    private GameObject parent;
+
+    private bool isActive = true;
+    void Start()
+    {
+        parent = gameObject.transform.parent.gameObject;
+    }
     public void HandleYesPressed()
     {
         Logger.Print("Yes button was pressed.");
-        // Add more logic here if needed
+        ToggleVisibility();
     }
 
     public void HandleNoPressed()
     {
         Logger.Print("No button was pressed.");
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToggleVisibility() {
+        if (isActive) {
+            parent.SetActive(false);
+            userInput.SetActive(true);
+            isActive = false;
+        } else {
+            parent.SetActive(true);
+            userInput.SetActive(false);
+            isActive = true;
+        }
+    
     }
 }
